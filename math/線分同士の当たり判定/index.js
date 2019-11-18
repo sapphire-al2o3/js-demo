01d1fafc95a22b1dc27b061f2e4bbd08b2086400
tel <t_telkun@hotmail.com>
2019-11-19 01:13:52 +0900

    反射の計算を修正

--- math/線分同士の当たり判定/index.js
+++ math/線分同士の当たり判定/index.js
@@ -1,13 +1,3 @@
-// 線分と線分の当たり判定
-function segToSeg(a, b) {
-    var v0 = a.end.sub(a.start),
-    c0 = b.start.sub(a.start).cross(v0),
-    c1 = b.end.sub(a.start).cross(v0),
-    v1 = b.end.sub(b.start),
-    c2 = a.start.sub(b.start).cross(v1),
-    c3 = a.end.sub(b.start).cross(v1);
-    return c0 * c1 < 0 && c2 * c3 < 0;
-}
 
 function rgba(r, g, b, a) {
     return 'rgba(' + [r, g, b, a].join(',') + ')';
@@ -37,6 +27,8 @@ function lerp(a, b, t) {
 var Particle = function(x, y) {
     this.x = x;
     this.y = y;
+    this.px = 0;
+    this.py = 0;
     this.vx = range(-1.0, 1.0);
     this.vy = range(-3.0, -0.0);
     this.a = 0.5;
@@ -79,7 +71,7 @@ Emitter.prototype = {
     emit: function() {
         var ps = this.particles;
         for(var i = 0; i < this.rate; i++) {
-            var x = this.x + range(-42.0, 42.0),
+            var x = this.x + range(-24.0, 24.0),
                 y = this.y + range(-8.0, 8.0),
                     p = new this.Particle(x, y);
             ps.push(p);
@@ -135,11 +127,38 @@ function distance(a, b) {
 
 var counter = document.getElementById('counter');
 
-var emitter = new Emitter(150, 50, Particle);
+var emitter = new Emitter(120, 50, Particle);
 emitter.rate = 5;
 
-//				var worker = new Worker('worker.js');
-//				worker.postMessage(emitter);
+// 線分と線分の当たり判定
+function segToSeg(a, b) {
+    var v0 = a.end.sub(a.start),
+        c0 = b.start.sub(a.start).cross(v0),
+        c1 = b.end.sub(a.start).cross(v0),
+        v1 = b.end.sub(b.start),
+        c2 = a.start.sub(b.start).cross(v1),
+        c3 = a.end.sub(b.start).cross(v1);
+    return c0 * c1 < 0 && c2 * c3 < 0;
+}
+
+// function segToSeg(p0, v0, p1, v1) {
+//     let c0 = (p1.x - p0.x) * v0.y - (p1.y - p0.y) * v0.x,
+//         c1 = b.end.sub(a.start).cross(v0),
+//         c2 = a.start.sub(b.start).cross(v1),
+//         c3 = a.end.sub(b.start).cross(v1);
+//     return c0 * c1 < 0 && c2 * c3 < 0;
+// }
+
+function interSeg(p0, v0, p1, v1) {
+    let d = v0.x * v1.y - v0.y * v1.x;
+    if (d === 0) {
+        return -1;
+    }
+
+    d = 1 / d;
+
+    return (v1.y * (p1.x - p0.x) - v1.x * (p1.y - p0.y)) * d;
+}
 
 // 2次方程式の解の公式
 function solve2(a, b, c) {
@@ -161,12 +180,12 @@ function solve2(a, b, c) {
 }
 
 function reflect(v, n, r) {
-	var d = (v.x * n.x + v.y * n.y) * 2.0;
-	r = r || {};
-	var s = 1 / (n.x * n.x + n.y * n.y);
-	r.x = v.x - d * n.x * s;
-	r.y = v.y - d * n.y * s;
-	return r;
+    var d = (v.x * n.x + v.y * n.y) * 2.0;
+    r = r || {};
+    var s = 1 / (n.x * n.x + n.y * n.y);
+    r.x = v.x - d * n.x * s;
+    r.y = v.y - d * n.y * s;
+    return r;
 }
 
 function Bezier2(p0, p1, p2) {
@@ -200,6 +219,14 @@ function bezier2Normal(p0, p1, p2, t) {
 	var x = 2 * (p2.x - 2 * p1.x + p0.x) * t + 2 * (p1.x - p0.x),
 		y = 2 * (p2.y - 2 * p1.y + p0.y) * t + 2 * (p1.y - p0.y);
 	return { x: y, y: -x };
+}
+
+function seg2Normal(v) {
+    return { x: v.y, y: -v.x };
+}
+
+function seg2Point(p, v, t) {
+    return { x: p.x + v.x * t, y: p.y + v.y * t };
 }
 
 // 二次ベジェ曲線と半直線の当たり判定
@@ -241,51 +268,43 @@ function inRect(p0x, p0y, p1x, p1y, px, py) {
 	return px >= minx && maxx >= px && py >= miny && maxy >= py;
 }
 
-function hit(p, p0, p1, p2) {
-	var v = { x: p.x - p.px, y: p.y - p.py };
-	var t = segToSeg(p0, p1, p2, v, { x: p.px, y: p.py });
-	
-	if(t <= 1.0 && t >= 0) {
-		var o = bezier2(p0, p1, p2, t);
-		
-		if(inRect(p.x, p.y, p.px, p.py, o.x, o.y)) {
-		
-			var n = bezier2Normal(p0, p1, p2, t);
-			var r = reflect(v, n);
-			var b = Math.random() * 0.2 + 0.4;
-			p.vx = r.x * b;
-			p.vy = r.y * b;
-			var lx = p.px - o.x,
-				ly = p.py - o.y;
-			var l = Math.sqrt(lx * lx + ly * ly);
-			r = normalize(r);
-			p.x = o.x + r.x * l;
-			p.y = o.y + r.y * l;
-			p.px = o.x;
-			p.py = o.y;
-		}
-	}
-	return t;
+function hit(p, v, seg, out) {
+    
+    // var t = segToSeg(p, v, seg.p, seg.v);
+    let t0 = interSeg(p, v, seg.p, seg.v),
+        t1 = interSeg(seg.p, seg.v, p, v);
+
+    // segToSeg()
+
+    if(t0 <= 1.0 && t0 >= 0 && t1 <= 1.0 && t1 >= 0) {
+        let o = seg2Point(p, v, t0);
+
+        let n = seg2Normal(seg.v);
+        
+        var r = reflect(v, n);
+        var b = Math.random() * 0.2 + 0.5;
+        out.vx = r.x * b;
+        out.vy = r.y * b;
+        var lx = p.x - o.x,
+            ly = p.y - o.y,
+            l = Math.sqrt(lx * lx + ly * ly);
+        r = normalize(r);
+        out.x = o.x + r.x * l;
+        out.y = o.y + r.y * l;
+        out.px = o.x;
+        out.py = o.y;
+    
+    }
 }
 
+let segments = [
+    { p: { x:  50, y:  75 }, v: { x: 100, y:  100 } },
+    { p: { x: 250, y: 275 }, v: { x: 100, y: -100 } },
+    { p: { x:  50, y: 275 }, v: { x: 100, y:  100 } }
+];
 
-
-var p0 = { x: 100, y: 300 },
-	p1 = { x: 200, y: 200 },
-	p2 = { x: 300, y: 300 };
-
-var seg = { x: 200, y: 200, vx: -50, vy: 200 };
-
-var t = hit(seg, p0, p1, p2);
-
-var p = bezier2(p0, p1, p2, t),
-	n = bezier2Normal(p0, p1, p2, t);
-
-let bar = [];
-bar.push({ start: { x: 50, y: 75 }, end: {x: 150, y: 175} });
-bar.push({ start: { x: 250, y: 275 }, end: {x: 350, y: 175} });
-bar.push({ start: { x: 50, y: 275 }, end: {x: 150, y: 375} });
-
+console.log(interSeg(segments[0].p, segments[0].v,
+    { x: 100, y: 100 }, { x: -100, y: 100 }));
 
 canvas.addEventListener('click', function(e) {
     emitter.emit();
@@ -296,9 +315,20 @@ setInterval(function() {
     emitter.update();
     emitter.particles.forEach(function(e, i) {
         //if(e.y > width) e.vy *= -1;
-        hit(e, bar);
+        // hit(e, bar);
         
-        if(e.x > 400 || e.y > 400 || e.x < 0) e.life = 0;
+        
+
+        let v = { x: e.x - e.px, y: e.y - e.py },
+            p = { x: e.px, y: e.py };
+
+        // hit(p, v, segments[0], e);
+
+        for(let i = 0; i < segments.length; i++) {
+            hit(p, v, segments[i], e);
+        }
+
+        if(e.x > width + 10 || e.y > height + 10 || e.x < -10) e.life = 0;
     });
     ctx.clearRect(0, 0, width, height);
     
@@ -306,10 +336,10 @@ setInterval(function() {
     
     ctx.strokeStyle = 'rgb(219, 33, 219)';
 
-    bar.forEach(e => {
+    segments.forEach(e => {
         ctx.beginPath();
-        ctx.moveTo(e.start.x, e.start.y);
-        ctx.lineTo(e.end.x, e.end.y);
+        ctx.moveTo(e.p.x, e.p.y);
+        ctx.lineTo(e.p.x + e.v.x, e.p.y + e.v.y);
         ctx.stroke();
     });
 
