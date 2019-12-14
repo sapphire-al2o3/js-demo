e203459851cd148c4de1736bd311df6dfa225280
tel <t_telkun@hotmail.com>
2019-12-14 17:28:23 +0900

    インデント修正

--- math/forked_-オイラー角→行列→オイラー角/index.js
+++ math/forked_-オイラー角→行列→オイラー角/index.js
@@ -13,13 +13,13 @@ function Matrix3() {
 
 Matrix3.rotateXYZ = function(x, y, z) {
     var m = new Matrix3(),
-	a = m.data,
-	cx = Math.cos(x),
-	sx = Math.sin(x),
-	cy = Math.cos(y),
-	sy = Math.sin(y),
-	cz = Math.cos(z),
-	sz = Math.sin(z);
+        a = m.data,
+        cx = Math.cos(x),
+        sx = Math.sin(x),
+        cy = Math.cos(y),
+        sy = Math.sin(y),
+        cz = Math.cos(z),
+        sz = Math.sin(z);
     
     a[0] = cy * cz;
     a[1] = sx * sy * cz - cx * sz;
@@ -38,36 +38,36 @@ Matrix3.rotateXYZ = function(x, y, z) {
 
 Matrix3.prototype.getRotateXYZ = function(c) {
     var a = this.data,
-	r = {};
+        r = {};
     if(a[6] === -1) {
-	r.y = Math.PI / 2;
-	throw 'hogehoge';
+        r.y = Math.PI / 2;
+        throw 'hogehoge';
     } else if(a[8] === 1) {
-	r.y = -Math.PI / 2;
-	throw 'hogehoge';
+        r.y = -Math.PI / 2;
+        throw 'hogehoge';
     } else {
-	r.x = Math.atan2(a[7], a[8]);
-	r.y = Math.asin(-a[6]);
-	r.z = Math.atan2(a[3], a[0]);
+        r.x = Math.atan2(a[7], a[8]);
+        r.y = Math.asin(-a[6]);
+        r.z = Math.atan2(a[3], a[0]);
     }
     return r;
 };
 
 Matrix3.prototype.getRotateXYZ_X = function(x) {
     var a = this.data,
-	r = {},
-	y = -a[6] - 1;
+        r = {},
+        y = -a[6] - 1;
     r.x = x;
     if(x === 0.0) {
-	r.y = Math.atan2(-a[6], a[8] / Math.cos(x));
+        r.y = Math.atan2(-a[6], a[8] / Math.cos(x));
     } else {
-	r.y = Math.atan2(-a[6], a[7] / Math.sin(x));
+        r.y = Math.atan2(-a[6], a[7] / Math.sin(x));
     }
     r.z = Math.atan2((a[1] + a[5]) / y, (a[2] - a[4]) / y) - x;
     
     if(y === 0.0) {
-	r.y = Math.PI * 0.5;
-	r.z = x - Math.atan2(a[1], a[2]);
+        r.y = Math.PI * 0.5;
+        r.z = x - Math.atan2(a[1], a[2]);
     }
     
     if (r.z > Math.PI) z -= 2 * Math.PI;
@@ -89,9 +89,9 @@ inx.onchange = iny.onchange = inz.onchange = function() {
 function calc(x, y, z) {
     var r = Matrix3.rotateXYZ(x, y, z);
     for(var i = 0; i < 3; i++) {
-	for(var j = 0; j < 3; j++) {
-	    $('m' + i + j + '').value = r.data[i * 3 + j].toFixed(3);
-	}
+        for(var j = 0; j < 3; j++) {
+            $('m' + i + j + '').value = r.data[i * 3 + j].toFixed(3);
+        }
     }
     var ir = r.getRotateXYZ_X(x);
     outx.textContent = (ir.x * TO_DEG).toFixed(3);
