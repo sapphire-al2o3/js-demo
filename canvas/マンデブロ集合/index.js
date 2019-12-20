e12a61fb0f16876471d6d95e25dd58eace95a700
tel <t_telkun@hotmail.com>
2019-12-20 23:30:42 +0900

    インデント修正

--- canvas/マンデブロ集合/index.js
+++ canvas/マンデブロ集合/index.js
@@ -4,20 +4,20 @@ var canvas = document.getElementById('canvas'),
 
 
 function mandelbrot(x, y) {
-  var n = 0,
-      zx = 0.0,
-      zy = 0.0,
-      t = 0.0;
-  while(n < 255) {
-    t = zx * zx - zy * zy + x;
-    zy = 2 * zx * zy + y;
-    zx = t;
-    if(zx * zx + zy * zy > 4) {
-      break;
+    var n = 0,
+        zx = 0.0,
+        zy = 0.0,
+        t = 0.0;
+    while(n < 255) {
+        t = zx * zx - zy * zy + x;
+        zy = 2 * zx * zy + y;
+        zx = t;
+        if(zx * zx + zy * zy > 4) {
+            break;
+        }
+        n++;
     }
-    n++;
-  }
-  return n;
+    return n;
 }
 console && console.time('draw');
 var x, y,
@@ -29,39 +29,30 @@ var x, y,
     m,
     zx, zy;
 for(var i = 0, h = img.height; i < h; i++) {
-  y = 2 * (i - cx) / h;
-  for(var j = 0, w = img.width; j < w; j++) {
-    x = 2 * (j - cy) / w - 0.5;
-    m = mandelbrot(x, y);
-    /*
-    m = zx = zy = 0;
-    while(m < 255) {
-      t = zx * zx - zy * zy + x;
-      zy = 2 * zx * zy + y;
-      zx = t;
-      if(zx * zx + zy * zy > 4) {
-        break;
-      }
-      m++;
+    y = 2 * (i - cx) / h;
+    for(var j = 0, w = img.width; j < w; j++) {
+        x = 2 * (j - cy) / w - 0.5;
+        m = mandelbrot(x, y);
+        /*
+        m = zx = zy = 0;
+        while(m < 255) {
+        t = zx * zx - zy * zy + x;
+        zy = 2 * zx * zy + y;
+        zx = t;
+        if(zx * zx + zy * zy > 4) {
+            break;
+        }
+        m++;
+        }
+        */
+        
+        data[index] = m;
+        data[index + 1] = m * 10 & 255;
+        data[index + 2] = m * 20 & 255;
+        data[index + 3] = 255;
+        
+        index += 4;
     }
-    */
-    
-    data[index] = m;
-    data[index + 1] = m * 10 & 255;
-    data[index + 2] = m * 20 & 255;
-    data[index + 3] = 255;
-    
-    index += 4;
-  }
 }
 console && console.timeEnd('draw');
 ctx.putImageData(img, x, y);
-
-  
-  
-  
-  
-  
-  
-  
-  
