6edf48128f99bd85a9f50e37e94b35fa13b50130
tel <t_telkun@hotmail.com>
2020-02-08 14:48:18 +0900

    キャプチャしてビデオに流す

--- canvas/Capture/index.js
+++ canvas/Capture/index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "#9BF";
+ctx.fillStyle = "#FFF";
 ctx.strokeStyle = "#9BF";
 ctx.lineWidth = 2;
 
@@ -12,7 +12,8 @@ let d = 0;
 
 setAnimationFrame((dt) => {
     time += dt;
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     x = Math.cos(time * 0.01);
     y = Math.sin(time * 0.01);
     d += dt * 0.1;
@@ -27,25 +28,33 @@ setAnimationFrame((dt) => {
     for(let i = -20; i < 20; i++) {
         ctx.beginPath();
         ctx.moveTo(0, i * 20 + d);
-        ctx.lineTo(i * 20 + 300 + d, 300);
+        ctx.lineTo(i * 20 + 320 + d, 320);
         ctx.stroke();
     }
-
-    // ctx.strokeRect(x, y, 100, 100);
 }, 1000 / 30);
 
+const video = document.getElementById('video');
+let data = [];
+let recorder;
+
+function stop() {
+    recorder.stop();
+    const buffer = new Blob(data, {type: 'video/webm'});
+    video.src = window.URL.createObjectURL(buffer);
+    video.controls = true;
+    document.getElementById('capture').disabled = false;
+}
+
 document.getElementById('capture').addEventListener('click', e => {
-    // let stream = canvas.captureStream(30);
-    // let recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
-    // let data = [];
-    // recorder.ondataavailable = event => data.push(event.data);
+    let stream = canvas.captureStream(30);
+    data.length = 0;
+    recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
+    recorder.ondataavailable = event => data.push(event.data);
+    recorder.onstop = stop;
+    recorder.start(100);
     document.getElementById('capture').disabled = true;
 }, false);
 
 document.getElementById('stop').addEventListener('click', e => {
-    // let stream = canvas.captureStream(30);
-    // let recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
-    // let data = [];
-    // recorder.ondataavailable = event => data.push(event.data);
-    document.getElementById('capture').disabled = false;
+    stop();
 }, false);
