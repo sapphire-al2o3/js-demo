8bd5b0ad000221c01d002315e7ba9ddf6f9920de
sapphire-al2o3 <t_telkun@hotmail.com>
2020-03-05 20:29:42 +0900

    バイナリからfloatへの変換処理を追加

--- math/float/index.js
+++ math/float/index.js
@@ -2,6 +2,8 @@
 const $f = document.getElementById('float');
 const $i = document.getElementById('int');
 
+convertF2I();
+
 function convertF2I()
 {
     let v = parseFloat($f.value);
@@ -12,12 +14,16 @@ function convertF2I()
 
 function convertI2F()
 {
-    let v = parseInt($f.value);
-    let floatArray = new Float32Array([v]);
-    let intArray = new Uint32Array(floatArray.buffer);
-    $i.value = intArray[0].toString(16);
+    let v = parseInt($i.value, 16);
+    let intArray = new Uint32Array([v]);
+    let floatArray = new Float32Array(intArray.buffer);
+    $f.value = floatArray[0];
 }
 
-document.getElementById('convert').addEventListener('click', e => {
+document.getElementById('f2i').addEventListener('click', e => {
     convertF2I();
+}, false);
+
+document.getElementById('i2f').addEventListener('click', e => {
+    convertI2F();
 }, false);
