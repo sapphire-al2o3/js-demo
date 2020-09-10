28b57c0da5d4aff8cfe9e4e3a021ea53d6eaafe8
sapphire-al2o3 <t_telkun@hotmail.com>
2020-09-10 23:30:45 +0900

    SNNフィルタのサンプル用ディレクトリ追加

---
+++ image-processing/SNN/index.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const img = document.getElementById('image');
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+};
