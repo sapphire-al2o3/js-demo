54aaf56aa5ef66515a14fe4ab994ec0c071d3d1b
sapphire-al2o3 <t_telkun@hotmail.com>
2020-09-25 12:59:37 +0900

    サンプル画像変更

--- image-processing/BilateralFilter/index.js
+++ image-processing/BilateralFilter/index.js
@@ -18,24 +18,27 @@ window.onload = () => {
     const weight = [];
 
     let radius = 3,
-        sigma = 0.5,
-        g = -0.5 / (sigma * sigma);
+        ssigma = 2,
+        csigma = 48;
 
     let width = image.width,
         height = image.height;
     
 
     let indices = [];
-    
+
+    const size = width * height;
+    const cs = -0.5 / (csigma * csigma);
+    const ss = -0.5 / (ssigma * ssigma);
+
     for(let i = -radius; i <= radius; i++) {
         for(let j = -radius; j <= radius; j++) {
-            let r = i * i + j * j;
-            weight.push(r * g);
-            indices.push(i * radius + j);
+            weight.push(Math.exp((i * i + j * j) * ss));
+            indices.push(i * width + j);
+            // console.log();
         }
     }
 
-    const size = width * height;
     for(let i = 0; i < size; i++) {
             
             let index = i * 4;
@@ -48,21 +51,27 @@ window.onload = () => {
 
             for(let k = 0; k < indices.length; k++) {
                 let t = index + indices[k] * 4;
+                if(t < 0 || t >= size * 4) continue;
                 let rr = data[t];
                 let gg = data[t + 1];
                 let bb = data[t + 2];
-
-                sr += rr;
-                sg += gg;
-                sb += bb;
-                sw += 0;
+                let dr = rr - r;
+                let dg = gg - g;
+                let db = bb - b;
+                let w = weight[k] * Math.exp((dr * dr + dg * dg + db * db) * cs);
+                // w = 1;
+                // w = Math.max(0.01, w);
+                sr += rr * w;
+                sg += gg * w;
+                sb += bb * w;
+                sw += w;
             }
 
             ret[index] = sr / sw ^ 0;
             ret[index + 1] = sg / sw ^ 0;
             ret[index + 2] = sb / sw ^ 0;
             ret[index + 3] = 255;
-        }
+        
     }
 
     ctx.putImageData(result, 0, 0);
