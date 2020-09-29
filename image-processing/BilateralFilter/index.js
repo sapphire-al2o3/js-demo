2d091206603e5fb8c8da7a3bfd6508f8dbe3e54b
sapphire-al2o3 <t_telkun@hotmail.com>
2020-09-29 23:50:23 +0900

    シグマを変更するスライダー追加

--- image-processing/BilateralFilter/index.js
+++ image-processing/BilateralFilter/index.js
@@ -15,7 +15,16 @@ window.onload = () => {
     const data = image.data;
     const ret = result.data;
 
-    const weight = [];
+   
+
+    document.body.appendChild(createSlider('color-sigma', 0.5, v => {
+        csigma = v * 96;
+    }));
+
+    document.body.appendChild(createSlider('space-sigma', 0.5, v => {
+        ssigma = v * 4;
+        render();
+    }));
 
     let radius = 3,
         ssigma = 2,
@@ -23,56 +32,61 @@ window.onload = () => {
 
     let width = image.width,
         height = image.height;
-    
 
-    let indices = [];
-
+    const weight = [];
+    const indices = [];
     const size = width * height;
-    const cs = -0.5 / (csigma * csigma);
-    const ss = -0.5 / (ssigma * ssigma);
 
-    for(let i = -radius; i <= radius; i++) {
-        for(let j = -radius; j <= radius; j++) {
-            weight.push(Math.exp((i * i + j * j) * ss));
-            indices.push(i * width + j);
-            // console.log();
-        }
-    }
+    function render() {
 
-    for(let i = 0; i < size; i++) {
-            
-            let index = i * 4;
-            let r = data[index];
-            let g = data[index + 1];
-            let b = data[index + 2];
+        let cs = -0.5 / (csigma * csigma);
+        let ss = -0.5 / (ssigma * ssigma);
 
-            let sr, sg, sb, sw;
-            sr = sg = sb = sw = 0;
+        weight.length = 0;
+        indices.length = 0;
 
-            for(let k = 0; k < indices.length; k++) {
-                let t = index + indices[k] * 4;
-                if(t < 0 || t >= size * 4) continue;
-                let rr = data[t];
-                let gg = data[t + 1];
-                let bb = data[t + 2];
-                let dr = rr - r;
-                let dg = gg - g;
-                let db = bb - b;
-                let w = weight[k] * Math.exp((dr * dr + dg * dg + db * db) * cs);
-                // w = 1;
-                // w = Math.max(0.01, w);
-                sr += rr * w;
-                sg += gg * w;
-                sb += bb * w;
-                sw += w;
+        for(let i = -radius; i <= radius; i++) {
+            for(let j = -radius; j <= radius; j++) {
+                weight.push(Math.exp((i * i + j * j) * ss));
+                indices.push(i * width + j);
             }
+        }
 
-            ret[index] = sr / sw ^ 0;
-            ret[index + 1] = sg / sw ^ 0;
-            ret[index + 2] = sb / sw ^ 0;
-            ret[index + 3] = 255;
-        
+        for(let i = 0; i < size; i++) {
+                
+                let index = i * 4;
+                let r = data[index];
+                let g = data[index + 1];
+                let b = data[index + 2];
+
+                let sr, sg, sb, sw;
+                sr = sg = sb = sw = 0;
+
+                for(let k = 0; k < indices.length; k++) {
+                    let t = index + indices[k] * 4;
+                    if(t < 0 || t >= size * 4) continue;
+                    let rr = data[t];
+                    let gg = data[t + 1];
+                    let bb = data[t + 2];
+                    let dr = rr - r;
+                    let dg = gg - g;
+                    let db = bb - b;
+                    let w = weight[k] * Math.exp((dr * dr + dg * dg + db * db) * cs);
+                    sr += rr * w;
+                    sg += gg * w;
+                    sb += bb * w;
+                    sw += w;
+                }
+
+                ret[index] = sr / sw ^ 0;
+                ret[index + 1] = sg / sw ^ 0;
+                ret[index + 2] = sb / sw ^ 0;
+                ret[index + 3] = 255;
+            
+        }
+
+        ctx.putImageData(result, 0, 0);
     }
 
-    ctx.putImageData(result, 0, 0);
+    render();
 };
