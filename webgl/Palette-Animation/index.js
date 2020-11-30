98a8c0fcb403bd9a4755404a2c09d894e2a6af04
tel <t_telkun@hotmail.com>
2020-12-01 00:34:48 +0900

    パレットをリピートにする

--- webgl/Palette-Animation/index.js
+++ webgl/Palette-Animation/index.js
@@ -177,8 +177,8 @@
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, paletteData.length / 3, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, paletteData);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 
     const indexLoc = gl.getUniformLocation(program, 'index');
     gl.uniform1i(indexLoc, 0);
