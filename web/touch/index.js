8a1b14e0c9c3e0d05b8f02e4f1890fa3574bb772
sapphire-al2o3 <t_telkun@hotmail.com>
2020-12-16 16:38:43 +0900

    タッチ入力のサンプル

---
+++ web/touch/index.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.body.addEventListener('touchstart', e => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    const touches = e.touches;
+    ctx.moveTo(touches[0].pageX, touches[0].pageY);
+    for (let i = 1; i < touches.length; i++) {
+        ctx.lineTo(touch.pageX, touch.pageY);
+        // ctx.arc(touch.pageX, touch.pageY, 4, 0, 2 * Math.PI, false);
+        // ctx.fill();
+    }
+    ctx.fill();
+}, false);
