2b846b0f8128a4077b103e0faffd4b269ddd8e47
tel <t_telkun@hotmail.com>
2021-02-07 23:02:58 +0900

    グリッド追加

--- math/Gaussian/index.js
+++ math/Gaussian/index.js
@@ -19,17 +19,40 @@ function plot2D(ctx, f, w, h, xr, yr, itr) {
     let y = f(x);
     ctx.moveTo(x * w / 2 + w / 2, h - y * h);
     
-    for (var i = 1 - itr; i <= itr; i++) {
+    for (let i = 1 - itr; i <= itr; i++) {
         x = i / itr * xr;
         y = f(x);
-        ctx.lineTo(x * w / 2 + w / 2, h - y * h);
+        ctx.lineTo(x * w / xr / 2 + w / 2, h - y * h);
     }
     ctx.stroke();
 }
 
+function grid(x, y) {
+    for (let i = 1; i < x; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * w / x + 0.5, 0);
+        ctx.lineTo(i * w / x + 0.5, h);
+        ctx.stroke();
+    }
+}
+
+let sigma = 1;
+let rangeX = 4;
+
 function render() {
     ctx.clearRect(0, 0, w, h);
-    plot2D(ctx, gauss.bind(null, 0.5), w, h, 1, 1, 200);
+    ctx.strokeStyle = '#AAA';
+    ctx.lineWidth = 1.0;
+    grid(rangeX, 1);
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 1.5;
+    plot2D(ctx, gauss.bind(null, sigma), w, h, rangeX, 1, 200);
+    
 }
+
+document.body.appendChild(createSlider('sigma', 1, v => {
+    sigma = v;
+    render();
+}));
 
 render();
