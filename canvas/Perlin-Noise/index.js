84083dcd9d019d51969023e42a6dc751303603d9
tel <t_telkun@hotmail.com>
2021-03-07 23:56:21 +0900

    パーリンノイズ実装

--- canvas/Perlin-Noise/index.js
+++ canvas/Perlin-Noise/index.js
@@ -18,3 +18,86 @@ const permutation = [
     49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
     138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
 ];
+
+function lerp(a, b, x) {
+    return a + x * (b - a);
+}
+
+function fade(t) {
+    return t * t * t * (t * (t * 6 - 15) + 10);
+}
+
+function grad(hash, x, y, z) {
+    let h = hash & 15;
+    let u = h < 8 ? x : y;
+    
+    let v;
+    if (h < 4) {
+        v = y;
+    } else if (h == 12 || h == 14) {
+        v = x;
+    } else {
+        v = z;
+    }
+    return ((h&1) == 0 ? u : -u)+((h&2) == 0 ? v : -v);
+}
+
+const p = [];
+
+for (let x = 0; x < 512; x++) {
+    p[x] = permutation[x % 256];
+}
+
+function noise(x, y, z) {
+    let X = Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
+        Y = Math.floor(y) & 255,                  // CONTAINS POINT.
+        Z = Math.floor(z) & 255;
+    x -= Math.floor(x);                                // FIND RELATIVE X,Y,Z
+    y -= Math.floor(y);                                // OF POINT IN CUBE.
+    z -= Math.floor(z);
+    let u = fade(x),                                // COMPUTE FADE CURVES
+        v = fade(y),                                // FOR EACH OF X,Y,Z.
+        w = fade(z);
+    let A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z,      // HASH COORDINATES OF
+        B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;      // THE 8 CUBE CORNERS,
+
+    return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),  // AND ADD
+                                   grad(p[BA  ], x-1, y  , z   )), // BLENDED
+                           lerp(u, grad(p[AB  ], x  , y-1, z   ),  // RESULTS
+                                   grad(p[BB  ], x-1, y-1, z   ))),// FROM  8
+                   lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),  // CORNERS
+                                   grad(p[BA+1], x-1, y  , z-1 )), // OF CUBE
+                           lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
+                                   grad(p[BB+1], x-1, y-1, z-1 ))));
+}
+
+function octavePerlin(x, y, z, octaves, persistence) {
+    let total = 0,
+        frequency = 1,
+        amplitude = 1,
+        maxValue = 0;
+    for (let i = 0; i < octaves; i++) {
+        total += noise(x * frequency, y * frequency, z * frequency) * amplitude;
+        
+        maxValue += amplitude;
+        
+        amplitude *= persistence;
+        frequency *= 2;
+    }
+    
+    return total / maxValue;
+}
+
+const image = ctx.createImageData(w, h);
+const data = image.data;
+
+for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+        let k = (i * w + j) * 4;
+        let y = noise(j / w, i / h, 0);
+        data[k] = data[k + 1] = data[k + 2] = y * 255 ^ 0;
+        data[k + 3] = 255;
+    }
+}
+
+ctx.putImageData(image, 0, 0);
