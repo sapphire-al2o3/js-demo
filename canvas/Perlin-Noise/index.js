5f1b1795ea56fed1cec6e60241d7bd700aad6a0e
tel <t_telkun@hotmail.com>
2021-03-08 23:21:21 +0900

    パーリンノイズのオクターブ合成

--- canvas/Perlin-Noise/index.js
+++ canvas/Perlin-Noise/index.js
@@ -19,6 +19,15 @@ const permutation = [
     138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
 ];
 
+let repeat = w;
+
+function inc(num) {
+    num++;
+    if (repeat > 0) num %= repeat;
+    
+    return num;
+}
+
 function lerp(a, b, x) {
     return a + x * (b - a);
 }
@@ -39,7 +48,7 @@ function grad(hash, x, y, z) {
     } else {
         v = z;
     }
-    return ((h&1) == 0 ? u : -u)+((h&2) == 0 ? v : -v);
+    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
 
 const p = [];
@@ -48,27 +57,51 @@ for (let x = 0; x < 512; x++) {
     p[x] = permutation[x % 256];
 }
 
-function noise(x, y, z) {
-    let X = Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
-        Y = Math.floor(y) & 255,                  // CONTAINS POINT.
-        Z = Math.floor(z) & 255;
-    x -= Math.floor(x);                                // FIND RELATIVE X,Y,Z
-    y -= Math.floor(y);                                // OF POINT IN CUBE.
-    z -= Math.floor(z);
-    let u = fade(x),                                // COMPUTE FADE CURVES
-        v = fade(y),                                // FOR EACH OF X,Y,Z.
-        w = fade(z);
-    let A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z,      // HASH COORDINATES OF
-        B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;      // THE 8 CUBE CORNERS,
+function perlin(x, y, z) {
+    if (repeat > 0) {
+        x = x % repeat;
+        y = y % repeat;
+        z = z % repeat;
+    }
+    
+    let xi = (x ^ 0) & 255;
+    let yi = (y ^ 0) & 255;
+    let zi = (z ^ 0) & 255;
+    let xf = x - (x ^ 0);
+    let yf = y - (y ^ 0);
+    let zf = z - (z ^ 0);
+    let u = fade(xf);
+    let v = fade(yf);
+    let w = fade(zf);
 
-    return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),  // AND ADD
-                                   grad(p[BA  ], x-1, y  , z   )), // BLENDED
-                           lerp(u, grad(p[AB  ], x  , y-1, z   ),  // RESULTS
-                                   grad(p[BB  ], x-1, y-1, z   ))),// FROM  8
-                   lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),  // CORNERS
-                                   grad(p[BA+1], x-1, y  , z-1 )), // OF CUBE
-                           lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
-                                   grad(p[BB+1], x-1, y-1, z-1 ))));
+    let aaa, aba, aab, abb, baa, bba, bab, bbb;
+    aaa = p[p[p[    xi ]+    yi ]+    zi ];
+    aba = p[p[p[    xi ]+inc(yi)]+    zi ];
+    aab = p[p[p[    xi ]+    yi ]+inc(zi)];
+    abb = p[p[p[    xi ]+inc(yi)]+inc(zi)];
+    baa = p[p[p[inc(xi)]+    yi ]+    zi ];
+    bba = p[p[p[inc(xi)]+inc(yi)]+    zi ];
+    bab = p[p[p[inc(xi)]+    yi ]+inc(zi)];
+    bbb = p[p[p[inc(xi)]+inc(yi)]+inc(zi)];
+
+    let x1, x2, y1, y2;
+    x1 = lerp(grad(aaa, xf,     yf, zf),
+              grad(baa, xf - 1, yf, zf),
+              u);
+    x2 = lerp(grad(aba, xf,     yf - 1, zf),
+              grad(bba, xf - 1, yf - 1, zf),
+              u);
+    y1 = lerp(x1, x2, v);
+
+    x1 = lerp(grad(aab, xf  , yf  , zf - 1),
+              grad(bab, xf - 1, yf, zf - 1),
+              u);
+    x2 = lerp(grad(abb, xf, yf - 1, zf - 1),
+              grad(bbb, xf - 1, yf - 1, zf - 1),
+              u);
+    y2 = lerp(x1, x2, v);
+    
+    return (lerp(y1, y2, w) + 1) / 2;
 }
 
 function octavePerlin(x, y, z, octaves, persistence) {
@@ -77,7 +110,7 @@ function octavePerlin(x, y, z, octaves, persistence) {
         amplitude = 1,
         maxValue = 0;
     for (let i = 0; i < octaves; i++) {
-        total += noise(x * frequency, y * frequency, z * frequency) * amplitude;
+        total += perlin(x * frequency, y * frequency, z * frequency) * amplitude;
         
         maxValue += amplitude;
         
@@ -94,7 +127,8 @@ const data = image.data;
 for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
         let k = (i * w + j) * 4;
-        let y = noise(j / w, i / h, 0);
+        let y = octavePerlin(j / w * 4, i / h * 4, 0, 8, 0.5);
+        // let y = perlin(i / w * 32, j / h * 32, 0);
         data[k] = data[k + 1] = data[k + 2] = y * 255 ^ 0;
         data[k + 3] = 255;
     }
