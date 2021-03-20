044b2441c3b080b57907c8b4f838143dd3a8dad0
tel <t_telkun@hotmail.com>
2021-03-20 23:54:02 +0900

    セルの位置を事前にもとめる

--- canvas/Cellular-Noise/index.js
+++ canvas/Cellular-Noise/index.js
@@ -5,64 +5,37 @@ const h = canvas.height;
 
 let repeat = w;
 
-function inc(num) {
-    num++;
-    if (repeat > 0) num %= repeat;
-    
-    return num;
+function dist2(sx, sy, ex, ey) {
+    return (ex - sx) * (ey - sy);
 }
 
-function lerp(a, b, x) {
-    return a + x * (b - a);
-}
+const b = 3;
+const bw = w / b ^ 0;
+const bh = h / b ^ 0;
+const randx = [];
+const randy = [];
 
-function fade(t) {
-    return t * t * t * (t * (t * 6 - 15) + 10);
-}
-
-const p = [];
-const permutation = [];
-
-for (let x = 0; x < 256; x++) {
-    permutation[x] = Math.random() * 256 ^ 0;
-}
-
-for (let x = 0; x < 512; x++) {
-    p[x] = permutation[x & 255];
-}
-
-function noise(x, y) {
-    
-    let xi = (x ^ 0) & 255;
-    let yi = (y ^ 0) & 255;
-    let xf = x - (x ^ 0);
-    let yf = y - (y ^ 0);
-    let u = fade(xf);
-    let v = fade(yf);
-
-    let a = p[p[xi] + yi];
-    let b = p[p[inc(xi)] + yi];
-    let c = p[p[xi] + inc(yi)];
-    let d = p[p[inc(xi)] + inc(yi)];
-    
-    return lerp(a, b, u) + (c - a) * v * (1 - u) + (d - b) * u * v;
-}
-
-function octave(x, y, octaves, persistence, frequency = 5) {
-    let total = 0,
-        amplitude = 1,
-        maxValue = 0;
-    for (let i = 0; i < octaves; i++) {
-        repeat = frequency;
-        total += noise(x * frequency, y * frequency) * amplitude;
-        
-        maxValue += amplitude;
-        
-        amplitude *= persistence;
-        frequency *= 2;
+for (let y = 0; y < bh; y++) {
+    for (let x = 0; x < bw; x++) {
+        let k = y * bw + x;
+        randx[k] = Math.random();
+        randy[k] = Math.random();
     }
+}
+
+function cell(x, y) {
+    let ix = x / b ^ 0;
+    let iy = y / b ^ 0;
+    let fx = x - ix;
+    let fy = y - iy;
     
-    return total / maxValue;
+    for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+            let k = (y + i) * bw + (x + j);
+            let cx = randx[k];
+            let cy = randy[k];
+        }
+    }
 }
 
 function render(data, octaves = 5, persistence = 0.5) {
@@ -70,8 +43,7 @@ function render(data, octaves = 5, persistence = 0.5) {
     for (let i = 0; i < h; i++) {
         for (let j = 0; j < w; j++) {
             let k = (i * w + j) * 4;
-            let y = octave(j / w, i / h, octaves, persistence);
-            // let y = noise(i / w * 32, j / h * 32, 0);
+            let y = cell(j, i);
             data[k] = data[k + 1] = data[k + 2] = y ^ 0;
             data[k + 3] = 255;
         }
@@ -85,16 +57,3 @@ const image = ctx.createImageData(w, h);
 const data = image.data;
 
 render(data);
-
-let octaves = 5,
-    persistence = 0.5;
-
-document.body.appendChild(createSlider('octaves', 5 / 8, v => {
-    octaves = (v * 8 ^ 0) + 1;
-    render(data, octaves);
-}));
-
-document.body.appendChild(createSlider('persistence', 0.5, v => {
-    persistence = v;
-    render(data, octaves, persistence);
-}));
