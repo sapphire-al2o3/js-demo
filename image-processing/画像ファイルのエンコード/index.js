119a34222146eeec1232f8c96eb49590c75d1cb2
tel <t_telkun@hotmail.com>
2021-05-01 23:54:25 +0900

    インデント修正

--- image-processing/画像ファイルのエンコード/index.js
+++ image-processing/画像ファイルのエンコード/index.js
@@ -1,19 +1,19 @@
 var area = document.getElementById('box');
 
 area.addEventListener('drop', function(e) {
-  e.preventDefault();
-  loadFile(e);
+    e.preventDefault();
+    loadFile(e);
 }, false);
 
 area.addEventListener('dragover', function(e) {
-  e.preventDefault();
+    e.preventDefault();
 }, false);
 
 function loadFile(e) {
-  var file = e.dataTransfer.files[0],
-      reader = new FileReader();
-  reader.onload = function(e) {
-    box.innerHTML = reader.result;
-  };
-  reader.readAsDataURL(file);
+    var file = e.dataTransfer.files[0],
+        reader = new FileReader();
+    reader.onload = function(e) {
+        box.innerHTML = reader.result;
+    };
+    reader.readAsDataURL(file);
 }
