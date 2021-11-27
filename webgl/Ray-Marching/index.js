c2fc9b6009335e81d2cbbebec5786d02bb5b457c
sapphire-al2o3 <t_telkun@hotmail.com>
2021-11-27 16:15:50 +0900

    ray marchingのサンプル追加

---
+++ webgl/Ray-Marching/index.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    let gl = initContext('canvas');
+
+    let program = [];
+
+    // シェーダを初期化
+    program.push(initShader(gl, 'shader-vs', 'shader-fs'));
+
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    
+    const s = 1;
+    const buffer = new Float32Array([
+        s, s,
+        -s, s,
+        s, -s,
+        -s, -s
+    ]);
+
+    let offset = [0, 0, 0];
+
+    gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
+
+    var loc = gl.getAttribLocation(program[0], 'position');
+    gl.enableVertexAttribArray(loc);
+    gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+    gl.useProgram(program[0]);
+
+    program[0].uniform['resolution'].value = [gl.canvas.width, gl.canvas.height];
+    setupUniform(program[0]);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    let frame = 0,
+        time = 0;
+
+    function render(delta) {
+        time += delta;
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        gl.flush();
+    }
+
+    let timer = setAnimationFrame(render, 1000 / 30);
+}());
