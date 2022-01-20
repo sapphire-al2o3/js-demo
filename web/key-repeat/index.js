da5d17105b0eb5fb426bc8648b23df0749f5b23a
sapphire-al2o3 <t_telkun@hotmail.com>
2022-01-20 17:53:15 +0900

    キーのトリガー処理作成

--- web/key-repeat/index.js
+++ web/key-repeat/index.js
@@ -1,1 +1,17 @@
 const count = document.getElementById('count');
+
+let down = false;
+let n = 0;
+
+document.body.addEventListener('keydown', e => {
+    
+    if (!down) {
+        n++;
+        count.textContent = n;
+    }
+    down = true;
+});
+
+document.body.addEventListener('keyup', e => {
+    down = false;
+});
