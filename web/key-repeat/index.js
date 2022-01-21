e8ca37e849a3c0a1d0cb649debaa1068419a5850
sapphire-al2o3 <t_telkun@hotmail.com>
2022-01-21 18:46:24 +0900

    リピート処理作成

--- web/key-repeat/index.js
+++ web/key-repeat/index.js
@@ -2,16 +2,38 @@ const count = document.getElementById('count');
 
 let down = false;
 let n = 0;
+let delay = 0.5;
+let interval = 0.05;
+let time = 0;
+let repeat = false;
+
+function countup() {
+    n++;
+    count.textContent = n;
+}
 
 document.body.addEventListener('keydown', e => {
     
     if (!down) {
-        n++;
-        count.textContent = n;
+        time = Date.now();
+        repeat = false;
+        countup();
+    } else {
+        const t = Date.now() - time;
+        if (repeat && t / 1000 > interval) {
+            countup();
+            time = Date.now();
+        } else if (!repeat && t / 1000 > delay) {
+            countup();
+            time = Date.now();
+            repeat = true;
+        }
+        
     }
     down = true;
 });
 
 document.body.addEventListener('keyup', e => {
     down = false;
+    repeat = false;
 });
