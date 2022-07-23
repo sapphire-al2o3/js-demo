46bdc9acc96a182b5cdd94a08cb49cd7ccee4605
tel <t_telkun@hotmail.com>
2022-07-23 23:58:21 +0900

    beginPathを忘れていたのを追加

--- math/Circle/index.js
+++ math/Circle/index.js
@@ -11,13 +11,16 @@ const scale = w / size;
 
 ctx.lineWidth = 1.0;
 ctx.strokeStyle = '#000';
+ctx.beginPath();
 ctx.arc(w / 2, h / 2, w / 2, 0, Math.PI * 2, true);
 ctx.stroke();
 
+ctx.beginPath();
 ctx.arc(w / 2, h / 2, w / 2 - 10, 0, Math.PI * 2, true);
 ctx.stroke();
 
 ctx.strokeStyle = '#AAA';
+ctx.beginPath();
 for (let i = 1; i < 16; i++) {
     let x = scale * i - 0.5;
     ctx.moveTo(0.5, x);
