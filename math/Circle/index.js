680e2900111b367f1bf0b4a6ad66d24d34917407
tel <t_telkun@hotmail.com>
2022-07-23 23:56:24 +0900

    円の描画サンプル

---
+++ math/Circle/index.js
@@ -0,0 +1,28 @@
+
+
+const canvas = document.getElementById('graph');
+const ctx = canvas.getContext('2d');
+
+const w = canvas.width;
+const h = canvas.height;
+
+const size = 16;
+const scale = w / size;
+
+ctx.lineWidth = 1.0;
+ctx.strokeStyle = '#000';
+ctx.arc(w / 2, h / 2, w / 2, 0, Math.PI * 2, true);
+ctx.stroke();
+
+ctx.arc(w / 2, h / 2, w / 2 - 10, 0, Math.PI * 2, true);
+ctx.stroke();
+
+ctx.strokeStyle = '#AAA';
+for (let i = 1; i < 16; i++) {
+    let x = scale * i - 0.5;
+    ctx.moveTo(0.5, x);
+    ctx.lineTo(w + 0.5, x);
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, h + 0.5);
+}
+ctx.stroke();
