f1b3dd1e9f3c910c680adc54c5607046f467bc81
tel <t_telkun@hotmail.com>
2022-09-08 00:10:15 +0900

    テキストを保存する

--- web/storage/index.js
+++ web/storage/index.js
@@ -1,12 +1,22 @@
 let date = localStorage.getItem('date');
+let text = localStorage.getItem('text');
 
-const text = document.getElementById('text');
-text.textContent = date;
+const $date = document.getElementById('date');
+if (date !== undefined) {
+    $date.textContent = date;
+}
+
+const $textarea = document.getElementById('textarea');
+if (text !== undefined) {
+    $textarea.value = text;
+}
 
 // ブラウザを閉じるときに保存する
 const save = () => {
     date = (new Date()).toString();
-    localStorage.setItem('date', date)
+    localStorage.setItem('date', date);
+
+    localStorage.setItem('text', $textarea.value);
 };
 
 window.addEventListener('pagehide', save, false);
