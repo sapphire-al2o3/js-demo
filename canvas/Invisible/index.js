79c15e565acfcd23e15116a80b358224c2063b13
sapphire-al2o3 <t_telkun@hotmail.com>
2022-10-25 19:51:19 +0900

    グリッド表示UI追加

--- canvas/Invisible/index.js
+++ canvas/Invisible/index.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d');
 const w = canvas.width;
 const h = canvas.height;
 
+let grid = true;
+
 function render() {
     ctx.fillStyle = '#FFF';
     ctx.fillRect(0, 0, w, h);
@@ -14,30 +16,33 @@ function render() {
     const ox = 16;
     const oy = 16;
 
-    for (let i = 0; i < 15; i++) {
-        ctx.beginPath();
-        ctx.moveTo(i * s + ox, 0);
-        ctx.lineTo(i * s + ox, h);
-        ctx.stroke();
+    if (grid) {
 
-        ctx.beginPath();
-        ctx.moveTo(0, i * s + oy);
-        ctx.lineTo(w, i * s + oy);
-        ctx.stroke();
-    }
+        for (let i = 0; i < 15; i++) {
+            ctx.beginPath();
+            ctx.moveTo(i * s + ox, 0);
+            ctx.lineTo(i * s + ox, h);
+            ctx.stroke();
 
-    for (let i = -10; i < 10; i++) {
-        ctx.beginPath();
-        ctx.moveTo(i * 2 * s + ox, oy - s * 2);
-        ctx.lineTo((i * 2 + 12) * s + ox, s * 10 + oy);
-        ctx.stroke();
-    }
+            ctx.beginPath();
+            ctx.moveTo(0, i * s + oy);
+            ctx.lineTo(w, i * s + oy);
+            ctx.stroke();
+        }
 
-    for (let i = -10; i < 10; i++) {
-        ctx.beginPath();
-        ctx.moveTo((i * 2 + 12) * s + ox, oy - s * 2);
-        ctx.lineTo(i * 2 * s + ox, s * 10 + oy);
-        ctx.stroke();
+        for (let i = -10; i < 10; i++) {
+            ctx.beginPath();
+            ctx.moveTo(i * 2 * s + ox, oy - s * 2);
+            ctx.lineTo((i * 2 + 12) * s + ox, s * 10 + oy);
+            ctx.stroke();
+        }
+
+        for (let i = -10; i < 10; i++) {
+            ctx.beginPath();
+            ctx.moveTo((i * 2 + 12) * s + ox, oy - s * 2);
+            ctx.lineTo(i * 2 * s + ox, s * 10 + oy);
+            ctx.stroke();
+        }
     }
 
     ctx.fillStyle = '#DDD';
@@ -58,5 +63,10 @@ function render() {
         }
     }
 }
+
+document.body.appendChild(createCheckbox('grid', (v, id) => {
+    grid = v;
+    render();
+}, true), false);
 
 render();
