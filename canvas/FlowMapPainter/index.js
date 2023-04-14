00c6ee36610257aa64213e4791b1d7a69ab1a0f5
sapphire-al2o3 <t_telkun@hotmail.com>
2023-04-14 23:22:17 +0900

    ペイント処理作成

--- canvas/FlowMapPainter/index.js
+++ canvas/FlowMapPainter/index.js
@@ -4,12 +4,20 @@ let img = new Image(),
     down = false,
     px = 0,
     py = 0,
-    strokeColor = 'rgba(200, 200, 255, 1.0)';
+    power = 16,
+    width = canvas[0].width,
+    height = canvas[0].height,
+    strokeColor = 'rgba(200, 200, 255, 1.0)',
+    baseColor = 'rgb(127, 127, 0)';
 
+// let image = ctx.getImageData(0, 0, canvas[0].width, canvas[0].height);
 
-ctx.lineWidth = 3.0;
+ctx.lineWidth = 32.0;
 ctx.strokeStyle = strokeColor;
 ctx.fillStyle = strokeColor;
+
+ctx.fillStyle = baseColor;
+ctx.fillRect(0, 0, width, height);
 
 canvas[0].addEventListener('mousedown', (e) => {
     const rect = e.target.getBoundingClientRect();
@@ -17,9 +25,28 @@ canvas[0].addEventListener('mousedown', (e) => {
     px = e.clientX - rect.left + 0.5;
     py = e.clientY - rect.top + 0.5;
     
-    ctx.beginPath();
-    ctx.moveTo(px, py);
+    // ctx.beginPath();
+    // ctx.moveTo(px, py);
 }, false);
+
+function clamp(x) {
+    return x > 1.0 ? 1.0 : x < -1.0 ? -1.0 : x;
+}
+
+function vecColor(dx, dy) {
+    
+    dx = clamp(dx * power / width);
+    dy = clamp(dy * power / height);
+
+    // normalize
+    // let l = dx * dx + dy * dy;
+    // dx /= l;
+    // dy /= l;
+
+    let r = (dx * 127 ^ 0) + 127;
+    let g = (dy * 127 ^ 0) + 127;
+    return `rgb(${r}, ${g}, 0)`;
+}
 
 canvas[0].addEventListener('mousemove', (e) => {
     if (down) {
@@ -27,7 +54,14 @@ canvas[0].addEventListener('mousemove', (e) => {
         let x = e.clientX - rect.left + 0.5,
             y = e.clientY - rect.top + 0.5;
         
-            ctx.lineTo(x, y);
+        let dx = px - x,
+            dy = py - y;
+        
+        ctx.strokeStyle = vecColor(dx, dy);
+
+        ctx.beginPath();
+        ctx.moveTo(px, py);
+        ctx.lineTo(x, y);
         ctx.stroke();
         
         px = x;
@@ -46,22 +80,23 @@ document.getElementById('clear').addEventListener('click', (e) => {
 }, false);
 
 function circle(x, y, r) {
-  ctx[3].arc(x, y, r, 0, Math.PI * 2.0, false);
+    ctx[3].arc(x, y, r, 0, Math.PI * 2.0, false);
 }
 
 function line(x, y, ex, ey) {
-  var seg = 20.0;
-  var dx = (ex - x) / seg,
-      dy = (ey - y) / seg;
-  ctx[3].beginPath();
-  for(var i = 0; i < seg; i++) {
-    circle(dx * i + x, dy * i + y, 1.5);
-  }
-  ctx[3].fill();
+    let seg = 20.0;
+    let dx = (ex - x) / seg,
+        dy = (ey - y) / seg;
+    ctx[3].beginPath();
+    for (let i = 0; i < seg; i++) {
+        circle(dx * i + x, dy * i + y, 1.5);
+    }
+    ctx[3].fill();
 }
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, 200, 200);
+    ctx.fillStyle = baseColor;
+    ctx.fillRect(0, 0, width, height);
 }
 
 function setCap(e) {
