b004f14bad3f1182a6e4eb94c9688716514d3ee9
sapphire-al2o3 <t_telkun@hotmail.com>
2023-05-13 21:10:49 +0900

    canvasに背景画像を指定するテスト

---
+++ web/CanvasBackground/index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+canvas.style.backgroundImage = 'url(penguin.jpg)';
