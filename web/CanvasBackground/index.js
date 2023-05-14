90369ec218b4bd7c30a32a6a0bd1f72447dc5fa8
sapphire-al2o3 <t_telkun@hotmail.com>
2023-05-14 22:48:37 +0900

    ボタンを押したとき背景を設定する

--- web/CanvasBackground/index.js
+++ web/CanvasBackground/index.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-canvas.style.backgroundImage = 'url(penguin.jpg)';
+document.getElementById('test').addEventListener('click', e => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.style.backgroundImage = 'url(penguin.jpg)';
+}, false);
