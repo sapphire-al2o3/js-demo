eb93ec5dd4d452b8ff2f9cc4ac3888cab94e4854
sapphire-al2o3 <t_telkun@hotmail.com>
2023-05-15 22:44:13 +0900

    fillRectしたときに表示されなくなる

--- web/CanvasBackground/index.js
+++ web/CanvasBackground/index.js
@@ -1,10 +1,16 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 400;
-canvas.height = 400;
+ctx.fillStyle = '#AAA';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 document.getElementById('test').addEventListener('click', e => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.style.backgroundImage = 'url(penguin.jpg)';
+}, false);
+
+
+document.getElementById('test2').addEventListener('click', e => {
+    canvas.width += 1;
+    canvas.width += -1;
 }, false);
