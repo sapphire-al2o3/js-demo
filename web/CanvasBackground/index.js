79e4f859f13db138f5670e750550ac72169dd6fd
sapphire-al2o3 <t_telkun@hotmail.com>
2023-05-15 16:12:38 +0900

    初期化時にサイズを変える

--- web/CanvasBackground/index.js
+++ web/CanvasBackground/index.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 400;
+canvas.height = 400;
+
 document.getElementById('test').addEventListener('click', e => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.style.backgroundImage = 'url(penguin.jpg)';
