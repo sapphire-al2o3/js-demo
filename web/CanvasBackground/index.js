bfa52deb0f048b02472f718153c8e3c21ba6f446
sapphire-al2o3 <t_telkun@hotmail.com>
2023-05-28 15:22:50 +0900

    半分だけ塗りつぶす

--- web/CanvasBackground/index.js
+++ web/CanvasBackground/index.js
@@ -8,6 +8,11 @@ document.getElementById('fill').addEventListener('click', e => {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }, false);
 
+document.getElementById('fill-half').addEventListener('click', e => {
+    ctx.fillStyle = '#AAA';
+    ctx.fillRect(0, 0, canvas.width, canvas.height / 2);
+}, false);
+
 document.getElementById('clear').addEventListener('click', e => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }, false);
