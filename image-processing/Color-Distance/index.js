a14f89b79338f64d51f59e66009f729645f86b28
sapphire-al2o3 <t_telkun@hotmail.com>
2023-07-24 21:22:36 +0900

    リファクタリング

--- image-processing/Color-Distance/index.js
+++ image-processing/Color-Distance/index.js
@@ -54,19 +54,19 @@ Color.prototype.toYCrCb = function() {
 };
 
 function randColor() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     return new Color(r, g, b);
 }
 
 function draw() {
     ctx.clearRect(0, 0, 400, 400);
     
-    var base = randColor();
+    let base = randColor();
     
-    var colors = [];
-    for(var i = 0; i < 15; i++) {
+    let colors = [];
+    for(let i = 0; i < 15; i++) {
         colors.push(randColor());
     }
     
@@ -76,10 +76,10 @@ function draw() {
     
     // SSD
     colors.cache = {};
-    colors.forEach(function(e) {
-        var dr = e.r - base.r;
-        var dg = e.g - base.g;
-        var db = e.b - base.b;
+    colors.forEach((e) => {
+        let dr = e.r - base.r;
+        let dg = e.g - base.g;
+        let db = e.b - base.b;
         this.cache[e.toString()] = dr * dr + dg * dg + db * db;
     }, colors);
     colors.sort(comp);
@@ -89,8 +89,8 @@ function draw() {
     ctx.fillStyle = '#000';
     ctx.fillText(base, 54, 24);
     
-    for(var i = 0; i < 15; i++) {
-        var c = colors[i].toString();
+    for(let i = 0; i < 15; i++) {
+        let c = colors[i].toString();
         ctx.fillStyle = c;
         ctx.fillRoundRect(16, 24 * i + 36, 32, 16, 6);
         ctx.fillStyle = '#000';
@@ -98,90 +98,90 @@ function draw() {
     }
     
     // SAD
-    colors.forEach(function(e) {
-        var dr = Math.abs(e.r - base.r);
-        var dg = Math.abs(e.g - base.g);
-        var db = Math.abs(e.b - base.b);
+    colors.forEach((e) => {
+        let dr = Math.abs(e.r - base.r);
+        let dg = Math.abs(e.g - base.g);
+        let db = Math.abs(e.b - base.b);
         this.cache[e.toString()] = dr + dg + db;
     }, colors);
     colors.sort(comp);
     
-    for(var i = 0; i < 15; i++) {
-        var c = colors[i].toString();
+    for(let i = 0; i < 15; i++) {
+        let c = colors[i].toString();
         ctx.fillStyle = c;
         ctx.fillRoundRect(120, 24 * i + 36, 32, 16, 6);
     }
     
     // NCC
-    var n = Math.sqrt(base.r * base.r + base.g * base.g + base.b * base.b);
-    colors.forEach(function(e) {
-        var d = e.r * base.r + e.g * base.g + e.b * base.b;
-        var en = Math.sqrt(e.r * e.r + e.g + e.g + e.b * e.b);
+    let n = Math.sqrt(base.r * base.r + base.g * base.g + base.b * base.b);
+    colors.forEach((e) => {
+        let d = e.r * base.r + e.g * base.g + e.b * base.b;
+        let en = Math.sqrt(e.r * e.r + e.g + e.g + e.b * e.b);
         colors.cache[e.toString()] = 1.0 - d / (en * n);
     });
     colors.sort(comp);
     
-    for(var i = 0; i < 15; i++) {
-        var c = colors[i].toString();
+    for(let i = 0; i < 15; i++) {
+        const c = colors[i].toString();
         ctx.fillStyle = c;
         ctx.fillRoundRect(180, 24 * i + 36, 32, 16, 6);
     }
     
     // ZNCC
     n = base.r * base.r + base.g * base.g + base.b * base.b;
-    var y = (base.r + base.g + base.b) / 3.0;
-    var r = base.r - y;
-    var g = base.g - y;
-    var b = base.b - y;
+    let y = (base.r + base.g + base.b) / 3.0;
+    let r = base.r - y;
+    let g = base.g - y;
+    let b = base.b - y;
     n = Math.sqrt(r * r + g * g + b * b);
     colors.forEach(function(e) {
-        var ey = (e.r + e.g + e.b) / 3.0;
-        var vr = (e.r - ey) * (base.r - y);
-        var vg = (e.g - ey) * (base.g - y);
-        var vb = (e.b - ey) * (base.b - y);
-        var d = vr + vg + vb;
-        var dr = e.r - ey;
-        var dg = e.g - ey;
-        var db = e.b - ey;
-        var en = Math.sqrt(dr * dr + dg * dg + db * db);
+        let ey = (e.r + e.g + e.b) / 3.0;
+        let vr = (e.r - ey) * (base.r - y);
+        let vg = (e.g - ey) * (base.g - y);
+        let vb = (e.b - ey) * (base.b - y);
+        let d = vr + vg + vb;
+        let dr = e.r - ey;
+        let dg = e.g - ey;
+        let db = e.b - ey;
+        let en = Math.sqrt(dr * dr + dg * dg + db * db);
         colors.cache[e.toString()] = 1.0 - d / (en * n);
     });
     colors.sort(comp);
-    for(var i = 0; i < 15; i++) {
+    for(let i = 0; i < 15; i++) {
         ctx.fillStyle = colors[i].toString();
         ctx.fillRoundRect(240, 24 * i + 36, 32, 16, 6);
     }
     
     // HSV
-    var hsv = base.toHSV();
-    colors.forEach(function(e) {
-        var ehsv = e.toHSV();
-        var dh = ehsv.h - hsv.h;
+    const hsv = base.toHSV();
+    colors.forEach((e) => {
+        let ehsv = e.toHSV();
+        let dh = ehsv.h - hsv.h;
         dh = dh > 180.0 ? 360.0 - dh : dh;
-        var ds = ehsv.s - hsv.s;
-        var dv = ehsv.v - hsv.v;
+        let ds = ehsv.s - hsv.s;
+        let dv = ehsv.v - hsv.v;
         colors.cache[e.toString()] = dh * dh + ds * ds + dv * dv;
     });
     colors.sort(comp);
     
-    for(var i = 0; i < 15; i++) {
-        var c = colors[i].toString();
+    for(let i = 0; i < 15; i++) {
+        const c = colors[i].toString();
         ctx.fillStyle = c;
         ctx.fillRoundRect(300, 24 * i + 36, 32, 16, 6);
     }
     
     // YCrCb
-    var c0 = base.toYCrCb();
-    colors.forEach(function(e) {
-        var c1 = e.toYCrCb();
-        var dy = c1.y - c0.y
-        var dcr = c1.cr - c0.cr;
-        var dcb = c1.cb - c0.cb;
+    const c0 = base.toYCrCb();
+    colors.forEach((e) => {
+        const c1 = e.toYCrCb();
+        let dy = c1.y - c0.y
+        let dcr = c1.cr - c0.cr;
+        let dcb = c1.cb - c0.cb;
         colors.cache[e.toString()] = dy * dy + dcr * dcr + dcb * dcb;
     });
     colors.sort(comp);
-    for(var i = 0; i < 15; i++) {
-        var c = colors[i].toString();
+    for(let i = 0; i < 15; i++) {
+        const c = colors[i].toString();
         ctx.fillStyle = c;
         ctx.fillRoundRect(360, 24 * i + 36, 32, 16, 6);
         //ctx.fillStyle = '#000';
