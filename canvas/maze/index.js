2ac864868ce7e0be39f1a8bd605b9155838a6d4d
sapphire-al2o3 <t_telkun@hotmail.com>
2023-10-18 22:49:56 +0900

    再生成するためのボタン追加

--- canvas/maze/index.js
+++ canvas/maze/index.js
@@ -6,72 +6,77 @@ const h = canvas.height;
 const TO_RAD = Math.PI / 180;
 const ROT_60 = TO_RAD * 60;
 
-const maze = [];
+let maze = [];
 let n = 31;
 let b = 8;
 
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        if (j === 0 || i === 0 || j === n - 1 || i === n - 1) {
-            maze.push(1);
-        } else {
-            maze.push(0);
+function boutaoshi() {
+    maze = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (j === 0 || i === 0 || j === n - 1 || i === n - 1) {
+                maze.push(1);
+            } else {
+                maze.push(0);
+            }
         }
     }
-}
 
-for (let i = 2; i < n - 2; i += 2) {
-    for (let j = 2; j < n - 2; j += 2) {
-        let k = i * n + j;
-        maze[k] = 1;
+    for (let i = 2; i < n - 2; i += 2) {
+        for (let j = 2; j < n - 2; j += 2) {
+            let k = i * n + j;
+            maze[k] = 1;
+        }
     }
-}
 
-function dir3(d) {
-    switch(d) {
-        case 0: return 1;
-        case 1: return n;
-        case 2: return -1;
+    function dir3(d) {
+        switch(d) {
+            case 0: return 1;
+            case 1: return n;
+            case 2: return -1;
+        }
+        return 0;
     }
-    return 0;
-}
 
-function dir4(d) {
-    switch(d) {
-        case 0: return 1;
-        case 1: return n;
-        case 2: return -n;
-        case 3: return -1;
+    function dir4(d) {
+        switch(d) {
+            case 0: return 1;
+            case 1: return n;
+            case 2: return -n;
+            case 3: return -1;
+        }
+        return 0;
     }
-    return 0;
-}
 
-for (let i = 2; i < n - 2; i += 2) {
-    let k = 2 * n + i;
-    let d = Math.random() * 4 ^ 0;
-    let w = k + dir4(d);
-    if (maze[w] === 0) {
-        maze[w] = 1;
-    } else {
-        d = Math.random() * 3 ^ 0;
-        w = k + dir4(d);
-        maze[w] = 1;
-    }
-}
-
-for (let i = 4; i < n - 2; i += 2) {
-    for (let j = 2; j < n - 2; j += 2) {
-        let k = i * n + j;
-        let d = Math.random() * 3 ^ 0;
-        let w = k + dir3(d);
+    for (let i = 2; i < n - 2; i += 2) {
+        let k = 2 * n + i;
+        let d = Math.random() * 4 ^ 0;
+        let w = k + dir4(d);
         if (maze[w] === 0) {
             maze[w] = 1;
         } else {
-            d = Math.random() * 2 ^ 0;
-            w = k + dir3(d);
+            d = Math.random() * 3 ^ 0;
+            w = k + dir4(d);
             maze[w] = 1;
         }
     }
+
+    for (let i = 4; i < n - 2; i += 2) {
+        for (let j = 2; j < n - 2; j += 2) {
+            let k = i * n + j;
+            let d = Math.random() * 3 ^ 0;
+            let w = k + dir3(d);
+            if (maze[w] === 0) {
+                maze[w] = 1;
+            } else {
+                d = Math.random() * 2 ^ 0;
+                w = k + dir3(d);
+                maze[w] = 1;
+            }
+        }
+    }
+
 }
 
 function render() {
@@ -89,7 +94,13 @@ function render() {
     }
 }
 
+boutaoshi();
 render();
+
+document.getElementById('generate').addEventListener('click', e => {
+    boutaoshi();
+    render();
+});
 
 const slider = createSlider('n', 1, v => {
 });
