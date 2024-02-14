21f7ed90931e901abbb20553361157e304ec9d5f
sapphire-al2o3 <t_telkun@hotmail.com>
2024-02-14 22:40:39 +0900

    bitパターンのサンプルの器だけ追加

---
+++ webgl/BitPattern/index.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    let gl = initContext2('canvas', { preserveDrawingBuffer : true });
+
+    let program = [];
+    program.push(initShader(gl, 'shader-vs', 'shader-fs'));
+
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    
+    const s = 1;
+    const buffer = new Float32Array([
+        s, s,
+        -s, s,
+        s, -s,
+        -s, -s
+    ]);
+
+    gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
+
+    const loc = gl.getAttribLocation(program[0], 'position');
+    gl.enableVertexAttribArray(loc);
+    gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+    gl.useProgram(program[0]);
+
+    setupUniform(program[0]);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    function render(delta) {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        gl.flush();
+    }
+
+    let timer = setAnimationFrame(render, 1000 / 30);
+
+    gl.canvas.addEventListener('click', () => {
+        timer.toggle();
+    });
+}());
