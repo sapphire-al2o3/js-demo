a952e738df91c20c4a526ef1ee2ce21ec2fa1136
sapphire-al2o3 <t_telkun@hotmail.com>
2024-06-05 15:35:12 +0900

    多重スクロール処理追加

--- game/sample-scroll/index.js
+++ game/sample-scroll/index.js
@@ -33,13 +33,31 @@ map.push(50);
 map.push(60);
 map.push(100);
 map.push(20);
+
+const sizef = 16;
+let mapf = [];
+let maxf = 16;
+for (let i = 0; i < maxf; i++) {
+    mapf.push(Math.random() * 100 + 10 ^ 0);
+}
 const max = map.length;
 let sx = 0;
 let speed = 2;
 
+let sxf = 0;
+let speedf = 1;
+
 function render() {
     ctx.clearRect(0, 0, W, H);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = '#777';
+    let kf = sxf / sizef ^ 0;
+    let of = sxf % sizef;
+    for (let i = 0; i < maxf; i++) {
+        let y = mapf[(kf + i) % maxf];
+        let x = i * sizef - of;
+        ctx.fillRect(x, y, sizef, H - y);
+    }
+    ctx.fillStyle = '#444';
     let k = sx / size ^ 0;
     let o = sx % size;
     for (let i = 0; i < max; i++) {
@@ -52,6 +70,7 @@ function render() {
 loop((dt) => {
 
     sx += speed;
+    sxf += speedf;
 
     render();
 
