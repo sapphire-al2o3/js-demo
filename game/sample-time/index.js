5bd3669f156fafcbef04960c1d4c50f0fcbb3627
sapphire-al2o3 <t_telkun@hotmail.com>
2024-06-20 20:20:40 +0900

    数字を表示する

--- game/sample-time/index.js
+++ game/sample-time/index.js
@@ -50,6 +50,39 @@ let rotSpeed = 0.2;
 
 const start = Date.now();
 
+const numPatterns = [
+    0x699996,
+    0x622222,
+    0x69124F,
+    0xE16196,
+    0x6AAAF2,
+    0xF8E11E,
+    0x68E996,
+    0xF12244,
+    0x696996,
+    0x699716
+];
+
+function fillNum(n, x, y, size) {
+    let d = n;
+    let charSize = size * 5;
+    let k = 0;
+    while (d > 0) {
+        let p = numPatterns[d % 10];
+        let b = 23;
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 4; j++) {
+                if ((p >> b) & 1) {
+                    ctx.fillRect(j * size + x - charSize * k, i * size + y, size, size);
+                }
+                b--;
+            }
+        }
+        d = d / 10 ^ 0;
+        k++;
+    }
+}
+
 loop((dt) => {
 
     const keyX = keyState['ArrowRight'] - keyState['ArrowLeft'];
@@ -82,5 +115,7 @@ loop((dt) => {
     ctx.font = '12px serif';
     ctx.textAlign = 'center';
     ctx.fillText(mtime / 1000 ^ 0, W / 2, H / 2);
+
+    fillNum(1234567890, 100, 10, 2);
 
 }, 1000 / 30);
