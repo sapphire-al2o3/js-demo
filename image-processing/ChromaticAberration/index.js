317d8417c5853055937ebe7acc6c44f5c9bb19f9
sapphire-al2o3 <t_telkun@hotmail.com>
2024-06-29 16:45:02 +0900

    オフセットを調整するUI追加

--- image-processing/ChromaticAberration/index.js
+++ image-processing/ChromaticAberration/index.js
@@ -20,26 +20,35 @@ window.onload = () => {
 
     let offset = 4;
 
-    for(let i = 0; i < h; i++) {
-        for(let j = 0; j < w; j++) {
-            let index = (i * w + j) * 4;
+    function render() {
+        for(let i = 0; i < h; i++) {
+            for(let j = 0; j < w; j++) {
+                let index = (i * w + j) * 4;
 
-            let or = j - offset;
-            if (or < 0) or = 0;
-            let ob = j + offset;
-            if (ob >= w) ob = w - 1;
+                let or = j - offset;
+                if (or < 0) or = 0;
+                let ob = j + offset;
+                if (ob >= w) ob = w - 1;
 
-            let r = data[(i * w + or) * 4];
-            let g = data[index + 1];
-            let b = data[(i * w + ob) * 4 + 2];
-            
-            
-            ret[index] = r;
-            ret[index + 1] = g;
-            ret[index + 2] = b;
-            ret[index + 3] = 255
+                let r = data[(i * w + or) * 4];
+                let g = data[index + 1];
+                let b = data[(i * w + ob) * 4 + 2];
+                
+                
+                ret[index] = r;
+                ret[index + 1] = g;
+                ret[index + 2] = b;
+                ret[index + 3] = 255
+            }
         }
+
+        ctx.putImageData(result, 0, 0);
     }
 
-    ctx.putImageData(result, 0, 0);
+    render();
+
+    document.body.appendChild(createSlider('offset', 0.5, v => {
+        offset = v * 8 ^ 0;
+        render();
+    }));
 };
