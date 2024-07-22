3162562c3cbafc74e1c10d26b6af007ccfe2dda7
sapphire-al2o3 <t_telkun@hotmail.com>
2024-07-22 15:11:21 +0900

    ディザのサンプル追加

---
+++ image-processing/Dithering/index.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const img = document.getElementById('image');
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+
+    const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const result = ctx.createImageData(canvas.width, canvas.height);
+    const data = image.data;
+    const ret = result.data;
+
+    let w = image.width,
+        h = image.height;
+
+    let bayer = [
+        0, 8, 2, 10,
+        12, 4, 14, 6,
+        3, 11, 1, 9,
+        15, 7, 13, 5
+    ];
+
+    bayer = bayer.map(v => v * 16 + 8);
+
+    function render() {
+
+        let src = data;
+        let dst = ret;
+
+        for(let i = 0; i < h; i++) {
+            for(let j = 0; j < w; j++) {
+                let index = (i * w + j) * 4;
+
+                let r = src[index];
+                let g = src[index + 1];
+                let b = src[index + 2];
+                let y = (r + g + b) / 3 ^ 0;
+
+                if (bayer[(i % 4) * 4 + (j % 4)] <= y) {
+                    y = 255;
+                } else {
+                    y = 0;
+                }
+                dst[index] = y;
+                dst[index + 1] = y;
+                dst[index + 2] = y;
+                dst[index + 3] = 255
+            }
+        }
+        ctx.putImageData(result, 0, 0);
+    }
+
+    render();
+};
