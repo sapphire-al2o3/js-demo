138f99f67e43e81940f40ecb6860663f242f6582
sapphire-al2o3 <t_telkun@hotmail.com>
2024-09-06 18:37:26 +0900

    頂点数を変えられるようにしておく

--- math/Centroid/index.js
+++ math/Centroid/index.js
@@ -6,9 +6,11 @@ let clickX = 0, clickY = 0;
 let down = false;
 let p0 = {x: 100, y: 100, label: 'P0'},
     p1 = {x: 300, y: 300, label: 'P1'},
-    p2 = {x: 300, y: 100, label: 'P2'};
+    p2 = {x: 300, y: 100, label: 'P2'},
+    p4 = {x: 100, y: 300, label: 'P4'};
 let active = {};
-let shapes = [p0, p1, p2];
+let shapes = [p0, p1, p2, p4];
+let count = 3;
 
 ctx.lineWidth = 2.0;
 ctx.font = '9pt consolas';
@@ -47,19 +49,25 @@ canvas.addEventListener('mousemove', e => {
 }, false);
 
 function center() {
-    let x = (p0.x + p1.x + p2.x) / 3;
-    let y = (p0.y + p1.y + p2.y) / 3;
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < count; i++) {
+        x += shapes[i].x;
+        y += shapes[i].y;
+    }
+    x /= count;
+    y /= count;
     return [x, y];
 }
 
 function draw() {
     let d = new Vector2(mouse.x, mouse.y);
     if (down) {
-        shapes.forEach(e => {
-            if (d.distance(e) < 6) {
-                active = e;
+        for (let i = 0; i < count; i++) {
+            if (d.distance(shapes[i]) < 6) {
+                active = shapes[i];
             }
-        });
+        }
         active.x = clickX;
         active.y = clickY;
     }
@@ -74,16 +82,18 @@ function draw() {
     ctx.lineTo(p0.x, p0.y);
     ctx.stroke();
     ctx.strokeStyle = '#000';
-    shapes.forEach(e => {
+    for (let i = 0; i < count; i++) {
+        const e = shapes[i];
         if (d.distance(e) < 6) {
             ctx.strokeCircle(e.x, e.y, 6);
         } else {
             ctx.strokeCircle(e.x, e.y, 3);
         }
         ctx.fillText(e.label, e.x, e.y - 8);
-    });
+    }
 
     let [x, y] = center();
+    ctx.strokeStyle = '#37E';
     ctx.strokeCircle(x, y, 3);
 }
 draw();
