77f491b6b700b8d807c5d16c540733537c05babd
sapphire-al2o3 <t_telkun@hotmail.com>
2024-09-30 19:11:27 +0900

    バタフライ曲線

---
+++ canvas/ButterflyCurve/index.js
@@ -0,0 +1,104 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+const centerX = width / 2;
+const centerY = height / 2;
+
+function hsl(h, s, l) { return 'hsl(' + h + ',' + s + '%,' + l + '%)'; }
+
+const setAnimFrame = (callback, interval) => {
+    let elapsed = 0,
+        time = Date.now(),
+        stop = false;
+    
+    const update = () => {
+        let delta = Date.now() - time;
+        time = Date.now();
+        elapsed += delta;
+        if (elapsed >= interval) {
+            var n = elapsed / interval ^ 0;
+            elapsed -= n * interval;
+            callback();
+        }
+        
+        if (!stop) {
+            requestAnimationFrame(update);
+        }
+    };
+    
+    update();
+    
+    return {
+        play: function() {
+            stop = false;
+            update();
+        },
+        pause: function() {
+            stop = true;
+        },
+        step: function() {
+            stop = true;
+            elapsed = interval;
+            update();
+        },
+        toggle: function() {
+            if(stop) {
+                this.play();
+            } else {
+                this.pause();
+            }
+        }
+    };
+};
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+let t = 0.0,
+    n = 16,
+    s = 0.01,
+    dot = false;
+
+function render() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for (let i = 0; i < n; i++) {
+        let r = Math.PI * 2.0 / n * i;
+        let th = t + r;
+        let z = Math.exp(Math.cos(th)) - 2 * Math.cos(4 * th) - Math.pow(Math.sin(th / 12), 5);
+        let x = centerX - Math.sin(th) * 50 * z,
+            y = centerY - Math.cos(th) * 50 * z;
+        th = th - s;
+        let pz = Math.exp(Math.cos(th)) - 2 * Math.cos(4 * th) - Math.pow(Math.sin(th / 12), 5);
+        let px = centerX - Math.sin(th) * 50 * pz,
+            py = centerY - Math.cos(th) * 50 * pz;
+        
+        if (dot) {
+            ctx.fillStyle = hsl(t * 100 ^ 0, 100, 50);
+            ctx.fillRect(x, y, 2, 2);
+        } else {
+            ctx.strokeStyle = hsl(t * 100 ^ 0, 100, 50);
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(px, py);
+            ctx.stroke();
+        }
+    }
+    t += s;
+}
+
+let hook = setAnimFrame(render, 1000 / 30),
+    playing = true;
+
+canvas.addEventListener('click', () => {
+    hook.toggle();
+}, false);
+
+
+document.body.appendChild(createSlider('n', n / 32, (v) => {
+    n = v * 32 ^ 0 + 1;
+}));
+document.body.appendChild(createCheckbox('dot', (v) => {
+    dot = v;
+}));
