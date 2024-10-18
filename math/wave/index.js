683391dfedc5ad87f28b2cf698402b1e991b41bf
sapphire-al2o3 <t_telkun@hotmail.com>
2024-10-18 18:17:30 +0900

    波形を切り替えるラジオボタン追加

--- math/wave/index.js
+++ math/wave/index.js
@@ -4,6 +4,7 @@ const w = canvas.width;
 const h = canvas.height;
 const PI2 = Math.PI * 2;
 const F = 32;
+let wave = 0;
 
 function clamp(x, min, max) {
     return x < min ? min : x > max ? max : x;
@@ -21,17 +22,20 @@ function render() {
     plot();
 }
 
+const offsetY = 0;
 function plot2D(ctx, f, w, h, xr, yr, itr) {
     itr = itr === undefined ? 100 : itr;
     ctx.beginPath();
     let x = -xr;
     let y = f(x);
-    ctx.moveTo(x * w / 2 + w / 2, h - y * h);
+    ctx.moveTo(x * w / 2 + w / 2, h - y * h + offsetY);
     
     for (let i = 1 - itr; i <= itr; i++) {
         x = i / itr * xr;
         y = f(x);
-        ctx.lineTo(x * w / xr / 2 + w / 2, h - y * h);
+        ctx.lineTo(x * w / xr / 2 + w / 2, h * (1 - y) + offsetY);
+        y = f(x + 0.0001);
+        // ctx.lineTo(x * w / xr / 2 + w / 2, h * (1 - y) + offsetY);
     }
     ctx.stroke();
 }
@@ -53,11 +57,18 @@ const squareWave = (x) => {
 function plot() {
     ctx.clearRect(0, 0, graph.width, graph.height);
     ctx.strokeStyle = '#000';
-    plot2D(ctx, sawtoothWave, graph.width, graph.height, 2, 1, 100);
-    ctx.strokeStyle = '#0F0';
-    plot2D(ctx, triangleWave, graph.width, graph.height, 2, 1, 100);
-    ctx.strokeStyle = '#00F';
-    plot2D(ctx, squareWave, graph.width, graph.height, 2, 1, 300);
+    if (wave === 0) {
+        plot2D(ctx, sawtoothWave, graph.width, graph.height, 2, 1, 100);
+    } else if (wave === 1) {
+        plot2D(ctx, triangleWave, graph.width, graph.height, 2, 1, 100);
+    } else if (wave === 2) {
+        plot2D(ctx, squareWave, graph.width, graph.height, 2, 1, 100);
+    }
 }
 
 render();
+
+document.body.appendChild(createRadio(['Triangle', 'Sawtooth', 'Square'], (v, id, i) => {
+    wave = i;
+    render();
+}));
