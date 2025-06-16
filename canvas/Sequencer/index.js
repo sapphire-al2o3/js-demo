8045f736bb8b4140e6ce1ad53794c9444e4d6ab7
sapphire-al2o3 <t_telkun@hotmail.com>
2025-06-16 19:33:59 +0900

    canvasのアニメーション

--- canvas/Sequencer/index.js
+++ canvas/Sequencer/index.js
@@ -1,10 +1,16 @@
 (function run() {
     'use strict';
     
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
     const box = document.getElementById("box");
     
     const text = run.toString();
     
+
+    ctx.strokeStyle = '#000';
+    ctx.fillStyle = '#844';
+
     // nフレーム待つ
     function* waitFrame(n) {
         for (; n; n--) {
@@ -21,11 +27,32 @@
     
     // ジェネレータを作る
     const i = function*() {
-        yield* waitTime(1000);
+        yield* waitTime(100);
         
-        for (let k = 0; k < text.length; k++) {
-            box.textContent += text[k];
-            yield* waitTime(50);
+        // for (let k = 0; k < text.length; k++) {
+        //     box.textContent += text[k];
+        //     yield* waitTime(50);
+        // }
+
+        for (let i = 0; i < 20; i++) {
+            let x = i * 20 + 0.5;
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, 400);
+            ctx.stroke();
+            yield* waitTime(100);
+        }
+        for (let i = 0; i < 20; i++) {
+            let y = i * 20 + 0.5;
+            ctx.moveTo(0, y);
+            ctx.lineTo(400, y);
+            ctx.stroke();
+            yield* waitTime(100);
+        }
+
+        for (let i = 0; i < 20; i++) {
+            let x = i * 20 + 0.5;
+            ctx.fillRect(x, 0, 20, 20);
+            yield* waitTime(200);
         }
     }();
     
