2770f18b3b207c396df3059b5e0fffdd43428197
sapphire-al2o3 <t_telkun@hotmail.com>
2025-07-14 20:27:46 +0900

    減衰方法を選べるようにする

--- web/WaveformSound/index.js
+++ web/WaveformSound/index.js
@@ -8,6 +8,8 @@ let buffer = context.createBuffer(
 
 let freq = 3000;
 let amp = 1;
+let fade = true;
+let ease = 0;
 
 function resize(length) {
     buffer = context.createBuffer(
@@ -17,6 +19,42 @@ function resize(length) {
     );
 }
 
+function linear(x) {
+    return 1 - x;
+}
+
+function quad(x) {
+    return 1 - x * x;
+}
+
+function bounce(x) {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+
+    if (x < 1 / d1) {
+        return 1 - (n1 * x * x);
+    } else if (x < 2 / d1) {
+        return 1 - (n1 * (x -= 1.5 / d1) * x + 0.75);
+    } else if (x < 2.5 / d1) {
+        return 1 - (n1 * (x -= 2.25 / d1) * x + 0.9375);
+    } else {
+        return 1 - (n1 * (x -= 2.625 / d1) * x + 0.984375);
+    }
+}
+
+function wind(x) {
+    switch (ease) {
+        case 0:
+            return linear(x);
+        case 1:
+            return quad(x);
+        case 2:
+            return bounce(x);
+        case 3:
+            return 1;
+    }
+}
+
 function setup() {
     for (let i = 0; i < buffer.numberOfChannels; i++) {
         const b = buffer.getChannelData(i);
@@ -24,7 +62,7 @@ function setup() {
         for (let j = 0; j < buffer.length; j++) {
             let t = j / l * time;
             let f = t * freq * Math.PI;
-            let w = 1 - j / l;
+            let w = wind(j / l);
             b[j] = amp * w * Math.sin(f);
         }
     }
@@ -36,6 +74,10 @@ document.body.appendChild(createSlider('freq', (freq + 100) / 10000, v => {
 
 document.body.appendChild(createSlider('amp', amp, v => {
     amp = v;
+}));
+
+document.body.appendChild(createRadio(['linear', 'quad', 'bounce', 'none'], (v, id, i) => {
+    ease = i;
 }));
 
 function play() {
