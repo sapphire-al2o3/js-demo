0c50d355bd1fe06eb34de671599dfa9577a09a13
sapphire-al2o3 <t_telkun@hotmail.com>
2025-08-06 21:42:44 +0900

    縞模様のテクスチャを適用する

--- webgl/Cellular/index.js
+++ webgl/Cellular/index.js
@@ -1,4 +1,15 @@
 'use strict';
+const stripe = document.getElementById('stripe');
+const ctx = stripe.getContext('2d');
+ctx.fillStyle = '#E00';
+ctx.fillRect(0, 0, 128, 8);
+ctx.fillStyle = '#000';
+for (let i = 0; i < 12; i++) {
+    let x = Math.random() * 128 ^ 0;
+    let w = Math.random() * 16 ^ 0;
+    ctx.fillRect(x, 0, w, 8);
+}
+
 
 let gl = initContext2('canvas', { preserveDrawingBuffer : true });
 
@@ -17,6 +28,7 @@ const buffer = new Float32Array([
 ]);
 
 let strength = 0.0;
+let p0 = [0.83,0.75];
 
 gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
 
@@ -24,8 +36,14 @@ const loc = gl.getAttribLocation(program[0], 'position');
 gl.enableVertexAttribArray(loc);
 gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
 
+let tex = initTexture(gl, stripe);
+program[0].uniform['tex'].value = 0;
+gl.bindTexture(gl.TEXTURE_2D, tex);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+
 gl.useProgram(program[0]);
-program[0].uniform['strength'].value = length
+program[0].uniform['strength'].value = strength;
+program[0].uniform['p0'].value = p0;
 setupUniform(program[0]);
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -33,7 +51,8 @@ let time = 0;
 function render(delta) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    program[0].uniform['strength'].value = strength
+    program[0].uniform['strength'].value = strength;
+    program[0].uniform['p0'].value = p0;
     setupUniform(program[0]);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     gl.flush();
@@ -48,6 +67,14 @@ gl.canvas.addEventListener('click', () => {
 });
 
 document.body.appendChild(createSlider('strength', 0, v => {
-    strength = v * 4;
+    strength = v * 8;
+}));
+
+document.body.appendChild(createSlider('p0.x', p0[0], v => {
+    p0[0] = v;
+}));
+
+document.body.appendChild(createSlider('p0.y', p0[1], v => {
+    p0[1] = v;
 }));
 
