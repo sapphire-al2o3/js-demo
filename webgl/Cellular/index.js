c1d14288cfc81e6501ab77eaa01c916f620f78b4
sapphire-al2o3 <t_telkun@hotmail.com>
2025-08-08 23:43:45 +0900

    セルラーノイズと切り替える

--- webgl/Cellular/index.js
+++ webgl/Cellular/index.js
@@ -1,6 +1,6 @@
 'use strict';
-const stripe = document.getElementById('stripe');
-const ctx = stripe.getContext('2d');
+const canvas = document.getElementById('stripeTex');
+const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#E00';
 ctx.fillRect(0, 0, 128, 8);
 ctx.fillStyle = '#000';
@@ -13,8 +13,7 @@ for (let i = 0; i < 12; i++) {
 
 let gl = initContext2('canvas', { preserveDrawingBuffer : true });
 
-let program = [];
-program.push(initShader(gl, 'shader-vs', 'shader-fs'));
+let program = initShader(gl, 'shader-vs', 'shader-fs');
 
 var vbo = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
@@ -31,36 +30,39 @@ let strength = 0.0;
 let p0 = [0.83,0.75];
 let p1 = [0.60,0.07];
 let p2 = [0.28,0.64];
+let stripe = true;
 
 gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.STATIC_DRAW);
 
-const loc = gl.getAttribLocation(program[0], 'position');
+const loc = gl.getAttribLocation(program, 'position');
 gl.enableVertexAttribArray(loc);
 gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
 
-let tex = initTexture(gl, stripe);
-program[0].uniform['tex'].value = 0;
+let tex = initTexture(gl, canvas);
+program.uniform['tex'].value = 0;
 gl.bindTexture(gl.TEXTURE_2D, tex);
 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 
-gl.useProgram(program[0]);
-program[0].uniform['strength'].value = strength;
-program[0].uniform['p0'].value = p0;
-program[0].uniform['p1'].value = p1;
-program[0].uniform['p2'].value = p2;
-setupUniform(program[0]);
+gl.useProgram(program);
+program.uniform['strength'].value = strength;
+program.uniform['p0'].value = p0;
+program.uniform['p1'].value = p1;
+program.uniform['p2'].value = p2;
+program.uniform['stripe'].value = stripe ? 1.0 : 0.0;
+setupUniform(program);
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 let time = 0;
 function render(delta) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    program[0].uniform['strength'].value = strength;
-    program[0].uniform['p0'].value = p0;
-    program[0].uniform['p1'].value = p1;
-    program[0].uniform['p2'].value = p2;
+    program.uniform['strength'].value = strength;
+    program.uniform['p0'].value = p0;
+    program.uniform['p1'].value = p1;
+    program.uniform['p2'].value = p2;
+    program.uniform['stripe'].value = stripe ? 1.0 : 0.0;
 
-    setupUniform(program[0]);
+    setupUniform(program);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     gl.flush();
 
@@ -68,6 +70,10 @@ function render(delta) {
 }
 
 let timer = setAnimationFrame(render, 1000 / 30);
+
+document.body.appendChild(createCheckbox('stripe', v => {
+    stripe = v;
+}, stripe));
 
 document.body.appendChild(createSlider('strength', 0, v => {
     strength = v * 8;
