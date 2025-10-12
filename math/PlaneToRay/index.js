651b7e49fe57fa5bf5f332ff39d00d52acce0d9e
sapphire-al2o3 <t_telkun@hotmail.com>
2025-10-12 22:00:21 +0900

    平面とレイの交点のディレクトリ追加

---
+++ math/PlaneToRay/index.js
@@ -0,0 +1,108 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let size = new Vector3(1, 1, 1);
+
+let proj = Matrix4.perspective(degToRad(30), 1, 0.1, 100),
+    target = new Vector3(0, 0, 0),
+    position = new Vector3(3, 4, 5),
+    view = Matrix4.lookAt(position, target, Vector3.up),
+    screen = Matrix4.viewport(canvas.width, canvas.height, 0.1, 100.0),
+    upMtx = screen.mul(proj),
+    upv = upMtx.mul(view);
+
+canvas.onmousewheel = (e) => {
+    let s = e.wheelDelta > 0 ? 1.0 / 1.1 : 1.1;
+    position = position.mul(s);
+    view = Matrix4.lookAt(position, target, Vector3.up);
+    upv = upMtx.mul(view);
+    render(size.x, size.y, size.z);
+};
+
+canvas.onmousedown = (e) => {
+    let x = e.clientX,
+        y = e.clientY;
+    canvas.onmousemove = (e) => {
+        // if(e.button === 0) {
+            let dx = x - e.clientX,
+                dy = y - e.clientY,
+                pos = position.rotate(new Vector3(0, 1, 0), dx * 0.01),
+                n = pos.cross(Vector3.up).normalize();
+            position = pos.rotate(n, -dy * 0.01);
+            view = Matrix4.lookAt(position, target, Vector3.up);
+            upv = upMtx.mul(view);
+            render(size.x, size.y, size.z);
+            x = e.clientX;
+            y = e.clientY;
+        // }
+    };
+        
+    canvas.onmouseup = (e) => {
+        canvas.oumouseup = null;
+        canvas.onmousemove = null;
+        canvas.onmouseout = null;
+    };
+    canvas.onmouseout = (e) => {
+        canvas.oumouseup = null;
+        canvas.onmousemove = null;
+        canvas.onmouseout = null;
+    };
+};
+	
+canvas.oncontextmenu = (e) => {
+	e.preventDefault();
+};
+
+ctx.lineWidth = 0.5;
+ctx.fillStyle = '#000';
+ctx.strokeStyle = "#FFF";
+ctx.clearRect(0, 0, 400, 400);
+render(size.x, size.y, size.z);
+
+function render(x, y, z) {
+    ctx.clearRect(0, 0, 400, 400);
+    
+    let p = [
+        new Vector3(-x,  y, -z),
+        new Vector3( x,  y, -z),
+        new Vector3( x,  y,  z),
+        new Vector3(-x,  y,  z),
+        new Vector3(-x, -y, -z),
+        new Vector3( x, -y, -z),
+        new Vector3( x, -y,  z),
+        new Vector3(-x, -y,  z),
+
+        new Vector3(0, 0, -5),
+        new Vector3(0, 0, 5),
+    ];
+  
+    p = p.map(e => upv.apply(e));
+    
+    line(p[0], p[1]);
+    line(p[1], p[2]);
+    line(p[2], p[3]);
+    line(p[3], p[0]);
+
+    line(p[4], p[5]);
+    line(p[5], p[6]);
+    line(p[6], p[7]);
+    line(p[7], p[4]);
+
+    line(p[0], p[4]);
+    line(p[1], p[5]);
+    line(p[2], p[6]);
+    line(p[3], p[7]);
+
+    line(p[8], p[9]);
+}
+
+function line(v0, v1) {
+    ctx.beginPath();
+    ctx.moveTo(v0.x, v0.y);
+    ctx.lineTo(v1.x, v1.y);
+    ctx.stroke();
+}
+
+function intersectPlaneToRay(p, n, o, r) {
+    return o.add(r.mul((p.dot(n) - n.dot(o)) / n.dot(r)));
+}
