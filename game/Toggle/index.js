341a4382ab6bbd16d5f6055a5e4d206528ab947b
sapphire-al2o3 <t_telkun@hotmail.com>
2025-11-10 00:39:45 +0900

    オフになっているボタンだけ押せるようにする

--- game/Toggle/index.js
+++ game/Toggle/index.js
@@ -1,9 +1,11 @@
 
 const map = [];
+const sizeX = 3;
+const sizeY = 3;
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < sizeY; i++) {
     map.push([]);
-    for (let j = 0; j < 3; j++) {
+    for (let j = 0; j < sizeX; j++) {
         map[i].push(0);
     }
 }
@@ -14,15 +16,16 @@ const cells = document.querySelectorAll('td');
 const mapElm = [];
 
 let k = 0;
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < sizeY; i++) {
     mapElm.push([]);
-    for (let j = 0; j < 3; j++) {
+    for (let j = 0; j < sizeX; j++) {
         mapElm[i].push(cells[k]);
         cells[k].setAttribute('x', j);
         cells[k].setAttribute('y', i);
         let r = Math.random() * 2 ^ 0;
         if (r === 1) {
             cells[k].classList.toggle('on');
+            map[i][j] = 1;
         }
         k++;
     }
@@ -32,18 +35,25 @@ table.addEventListener('click', e => {
     if (e.target.tagName === 'TD') {
         let x = parseInt(e.target.getAttribute('x'));
         let y = parseInt(e.target.getAttribute('y'));
-        mapElm[y][x].classList.toggle('on');
-        if (y > 0) {
-            mapElm[y - 1][x].classList.toggle('on');
-        }
-        if (y < 2) {
-            mapElm[y + 1][x].classList.toggle('on');
-        }
-        if (x > 0) {
-            mapElm[y][x - 1].classList.toggle('on');
-        }
-        if (x < 2) {
-            mapElm[y][x + 1].classList.toggle('on');
+        if (map[y][x] === 0) {
+            mapElm[y][x].classList.toggle('on');
+            map[y][x] = 1;
+            if (y > 0) {
+                mapElm[y - 1][x].classList.toggle('on');
+                map[y - 1][x] = 1 - map[y - 1][x];
+            }
+            if (y < sizeY - 1) {
+                mapElm[y + 1][x].classList.toggle('on');
+                map[y + 1][x] = 1 - map[y + 1][x];
+            }
+            if (x > 0) {
+                mapElm[y][x - 1].classList.toggle('on');
+                map[y][x - 1] = 1 - map[y][x - 1];
+            }
+            if (x < sizeX - 1) {
+                mapElm[y][x + 1].classList.toggle('on');
+                map[y][x + 1] = 1 - map[y][x + 1];
+            }
         }
 
         // e.target.classList.toggle('on');
