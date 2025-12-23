8f64c16428261ffc8c5f25e471eb15fe56c34f48
sapphire-al2o3 <t_telkun@hotmail.com>
2025-12-23 23:45:15 +0900

    y座標をもとめる関数をつくる

--- math/Curve/index.js
+++ math/Curve/index.js
@@ -59,6 +59,25 @@ function bezier3(p0, p1, p2, p3, t) {
     };
 }
 
+function evaluate(p0, p1, p2, p3, x) {
+    let p1x = p0.x;
+    let p1y = p0.y;
+    let tp1 = (p1.y - p0.y) / (p1.x - p0.x);
+    let p2x = p3.x;
+    let p2y = p3.y;
+    let tp2;
+    if (p3.x === p2.x) {
+        tp2 = 1;
+    } else {
+        tp2 = (p3.y - p2.y) / (p3.x - p2.x);
+    }
+    let a = (p1x * tp1 + p1x * tp2 - p2x * tp1 - p2x * tp2 - 2 * p1y + 2 * p2y) / (p1x * p1x * p1x - p2x * p2x * p2x + 3 * p1x * p2x * p2x - 3 * p1x * p1x * p2x);
+    let b = ((-p1x * p1x * tp1 - 2 * p1x * p1x * tp2 + 2 * p2x * p2x * tp1 + p2x * p2x * tp2 - p1x * p2x * tp1 + p1x * p2x * tp2 + 3 * p1x * p1y - 3 * p1x * p2y + 3 * p1y * p2x - 3 * p2x * p2y) / (p1x * p1x * p1x - p2x * p2x * p2x + 3 * p1x * p2x * p2x - 3 * p1x * p1x * p2x));
+    let c = ((p1x * p1x * p1x * tp2 - p2x * p2x * p2x * tp1 - p1x * p2x * p2x * tp1 - 2 * p1x * p2x * p2x * tp2 + 2 * p1x * p1x * p2x * tp1 + p1x * p1x * p2x * tp2 - 6 * p1x * p1y * p2x + 6 * p1x * p2x * p2y) / (p1x * p1x * p1x - p2x * p2x * p2x + 3 * p1x * p2x * p2x - 3 * p1x * p1x * p2x));
+    let d = ((p1x * p2x * p2x * p2x * tp1 - p1x * p1x * p2x * p2x * tp1 + p1x * p1x * p2x * p2x * tp2 - p1x * p1x * p1x * p2x * tp2 - p1y * p2x * p2x * p2x + p1x * p1x * p1x * p2y + 3 * p1x * p1y * p2x * p2x - 3 * p1x * p1x * p2x * p2y) / (p1x * p1x * p1x - p2x * p2x * p2x + 3 * p1x * p2x * p2x - 3 * p1x * p1x * p2x));
+    return a * x * x * x + b * x * x + c * x + d;
+}
+
 function fillCircle(x, y, r) {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2, false);
@@ -126,6 +145,15 @@ function draw() {
     // }
     // ctx.lineTo(p3.x, p3.y);
     // ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(p0.x, p0.y);
+    for(let i = 1; i < 200; i++) {
+        let p = evaluate(p0, p1, p2, p3, i + p0.x);
+        ctx.lineTo(p, p0.y + i);
+    }
+    ctx.lineTo(p3.x, p3.y);
+    ctx.stroke();
 
 
     ctx.lineWidth = 0.5;
