17d7b4a4afc2c82ffd4cdbcd86595839f98d1d2b
sapphire-al2o3 <t_telkun@hotmail.com>
2025-12-25 22:43:29 +0900

    Boidのサンプル用のディレクトリ追加

---
+++ canvas/Boid/index.js
@@ -0,0 +1,136 @@
+// https://note.com/anglers_member/n/nb00eca1226fc
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const w = canvas.width;
+const h = canvas.height;
+
+const boids = [];
+const Max = 30;
+
+let sw = 0.0002;
+let aw = 0.0002;
+let cw = 0.0002;
+
+for (let i = 0; i < Max; i++) {
+    boids.push({
+        x: Math.random() * w,
+        y: Math.random() * h,
+        vx: Math.random(),
+        vy: Math.random(),
+        dx: 1,
+        dy: 0
+    });
+}
+
+
+function rand(min, max) {
+    return Math.random() * (max - min + 1) + min ^ 0;
+}
+
+function clamp(x, min, max) {
+    return x < min ? min : x > max ? max : x;
+}
+
+function separation(k) {
+    let fx = 0;
+    let fy = 0;
+    let p = boids[k];
+    for (let i = 0; i < boids.length; i++) {
+        if (i !== k) {
+            let x = p.x - boids[i].x;
+            let y = p.x - boids[i].y;
+            let l = x * x + y * y;
+            fx += x / l;
+            fy += y / l;
+        }
+    }
+    return [fx, fy];
+}
+
+function alignment(k) {
+    let vx = 0;
+    let vy = 0;
+    for (let i = 0; i < boids.length; i++) {
+        if (i !== k) {
+            vx += boids[i].vx;
+            vy += boids[i].vy;
+        }
+    }
+    let p = boids[k];
+    let n = boids.length - 1;
+    vx = vx / n - p.vx;
+    vy = vy / n - p.vy;
+    return [vx, vy];
+}
+
+function cohesion(k) {
+    let cx = 0;
+    let cy = 0;
+    for (let i = 0; i < boids.length; i++) {
+        if (i !== k) {
+            cx += boids[i].x;
+            cy += boids[i].y;
+        }
+    }
+    let p = boids[k];
+    let n = boids.length - 1;
+    cx = cx / n - p.vx;
+    cy = cy / n - p.vy;
+    return [cx, cy];
+}
+
+function update() {
+
+
+
+    for (let i = 0; i < boids.length; i++) {
+
+        let [sx, sy] = separation(i);
+        let [ax, ay] = alignment(i);
+        let [cx, cy] = cohesion(i);
+
+        boids[i].vx += sw * sx + aw * ax + cw * cx;
+        boids[i].vy += sw * sy + aw * ay + cw * cy;
+
+        boids[i].x += boids[i].vx;
+        boids[i].y += boids[i].vy;
+    }
+}
+
+function render() {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.fillStyle ='#FFF';
+    for (let i = 0; i < boids.length; i++) {
+        let x = boids[i].x;
+        let y = boids[i].y;
+        ctx.fillRect(x, y, 4, 4);
+    }
+}
+
+setAnimationFrame((t) => {
+    update();
+    render();
+}, 1000 / 30);
+
+// document.body.appendChild(createCheckbox('Area', v => {
+//     area = v;
+//     render();
+// }, true));
+
+// document.body.appendChild(createCheckbox('Stairs', v => {
+//     stairs = v;
+//     render();
+// }, true));
+
+// document.body.appendChild(createButton('generate', () => {
+//     fillMap(0, 0, sizeW, sizeH, 0)
+//     root = split(treeDepth, 0, 0, sizeW, sizeH);
+//     room(root);
+//     corridor(root, 0);
+//     placement(root, 2, 0, 0);
+//     placement(root, 3, 0, rand(1, (1 << treeDepth) - 1));
+//     render();
+// }), false);
