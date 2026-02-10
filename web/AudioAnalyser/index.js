e8bfd17f8bcb4f26806f83cd4f9818e4eed23aac
sapphire-al2o3 <t_telkun@hotmail.com>
2026-02-10 22:47:25 +0900

    時間経過で位置をずらしながら描画する

--- web/AudioAnalyser/index.js
+++ web/AudioAnalyser/index.js
@@ -11,41 +11,66 @@ const timeDomain = new Uint8Array(bufferLength);
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let frame = 0;
+
 function render() {
     analyser.getByteTimeDomainData(timeDomain);
     
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = "#FFF";
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000";
 
-    ctx.beginPath();
-
-    const sliceWidth = (canvas.width * 1.0) / bufferLength;
-    let x = 0;
-
-    for (let i = 0; i < bufferLength; i++) {
-        const v = timeDomain[i] / 128.0;
-        const y = (v * canvas.height) / 2;
-
-        if (i === 0) {
-            ctx.moveTo(x, y);
-        } else {
-            ctx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
+    let min = 1000;
+    let max = 0;
+    let r = 0;
+    for(let i = 0; i < timeDomain.length; i++) {
+        let d = timeDomain[i] - 128;
+        let t = Math.abs(timeDomain[i] - 128);
+        r += t;
+        if (d < min) min = d;
+        if (d > max) max = d;
     }
+    r /= timeDomain.length;
 
-    ctx.lineTo(canvas.width, canvas.height / 2);
-    ctx.stroke();
+    // console.log(r);
+
+    ctx.fillStyle = 'rgb(0 0 0)';
+    ctx.fillRect(frame, canvas.height * 0.5 - max, 1, min);
+
+    // ctx.beginPath();
+
+    // const sliceWidth = (canvas.width * 1.0) / bufferLength;
+    // let x = 0;
+
+    // for (let i = 0; i < bufferLength; i++) {
+    //     const v = timeDomain[i] / 128.0;
+    //     const y = (v * canvas.height) / 2;
+
+    //     if (i === 0) {
+    //         ctx.moveTo(x, y);
+    //     } else {
+    //         ctx.lineTo(x, y);
+    //     }
+
+    //     x += sliceWidth;
+    // }
+
+    // ctx.lineTo(canvas.width, canvas.height / 2);
+    // ctx.stroke();
+
+    frame++;
+    if (frame > 400) {
+        reset();
+    }
 
     if (!audio.ended) {
         requestAnimationFrame(render);
     } else {
         playButton.disabled = false;
         source.disconnect();
+        console.log(frame);
     }
 }
 
@@ -94,7 +119,14 @@ document.addEventListener('drop', (e) => {
 let playing = false;
 const playButton = document.getElementById('play');
 
+function reset() {
+    frame = 0;
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 playButton.addEventListener('click', (e) => {
+    reset();
     play();
     render();
     playButton.disabled = true;
