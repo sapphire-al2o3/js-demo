58adbe0164f74d0a8c0ab3366bb463bded365633
sapphire-al2o3 <t_telkun@hotmail.com>
2026-03-08 20:30:30 +0900

    色を補間して設定する

--- canvas/円錐グラデーション/index.js
+++ canvas/円錐グラデーション/index.js
@@ -3,6 +3,16 @@ const ctx = canvas.getContext('2d');
 let time = 0;
 function rgba(r, g, b, a) { return 'rgba(' + (r^0) + ',' + g + ',' + b + ',' + a + ')'; };
 
+let color0 = [255, 0, 0];
+let color1 = [0, 0, 0];
+
+function getColor(a) {
+    let r = color0[0] * a + color1[0] * (1 - a);
+    let g = color0[1] * a + color1[1] * (1 - a);
+    let b = color0[2] * a + color1[2] * (1 - a);
+    return rgba(r, g, b, 1);
+}
+
 const timer = setAnimationFrame(() => {
     ctx.clearRect(0, 0, 400, 400);
     var d = 200;
@@ -14,8 +24,8 @@ const timer = setAnimationFrame(() => {
             x1 = Math.cos(r1) * 200 + 200,
             y1 = Math.sin(r1) * 200 + 200;
         let grad3 = ctx.createLinearGradient(x0, y0, x1, y1);
-        grad3.addColorStop(0, rgba(255 * i / d, 0, 0, 1));
-        grad3.addColorStop(1, rgba(255 * (i + 1) / d, 0, 0, 1));
+        grad3.addColorStop(0, getColor(i / d));
+        grad3.addColorStop(1, getColor((i + 1) / d));
         ctx.fillStyle = grad3;
         ctx.beginPath();
         ctx.moveTo(200, 200);
@@ -23,8 +33,9 @@ const timer = setAnimationFrame(() => {
         ctx.fill();
     }
     time += 1.0;
-}, 1000/30);
+}, 1000 / 30);
 
 canvas.addEventListener('click', e => {
     timer.toggle();
 }, false);
+
