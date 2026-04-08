6ae1dcfe0d53f1c2e3ec8485f80c8621384b8536
sapphire-al2o3 <t_telkun@hotmail.com>
2026-04-09 00:05:35 +0900

    PNGのアイコン表示

--- web/ico/index.js
+++ web/ico/index.js
@@ -1,6 +1,18 @@
-
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const maxSize = 128 * 1024;
 const content = document.getElementById('content');
+
+async function loadPng(buffer, x) {
+    const blob = new Blob([buffer], { type: 'image/png' });
+    const image = await createImageBitmap(blob);
+    ctx.drawImage(image, x, 0);
+    image.close();
+}
+
+function loadBmp(buffer, x) {
+
+}
 
 async function load(file) {
     const buffer = await file.arrayBuffer();
@@ -26,6 +38,8 @@ async function load(file) {
     // IconInfoHeader
     offset += 6;
 
+    let files = [];
+
     for (let i = 0; i < count; i++) {
         let width = dataView.getUint8(offset, true);
         let height = dataView.getUint8(offset + 1, true);
@@ -44,8 +58,38 @@ async function load(file) {
         texts.push(`Width: ${width}`);
         texts.push(`Height: ${height}`);
         texts.push(`ColorCount: ${colorCount}`);
+        texts.push(`Size: ${dibSize}`);
+        texts.push(`Offset: 0x${dibOffset.toString(16)}`);
+
+        files.push({
+            size: dibSize,
+            offset: dibOffset,
+            width: width
+        });
     }
     content.textContent = texts.join('\n');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let offsetX = 0;
+    for (let i = 0; i < files.length; i++) {
+        offset = files[i].offset;
+        let s = dataView.getUint8(offset, true);
+        if (s === 0x89) {
+            // PNG
+            loadPng(new Uint8Array(buffer, files[i].offset, files[i].size), offsetX);
+        } else if (s === 40) {
+            let bcWidth = dataView.getInt32(offset + 4, true);
+            let bcHeight = dataView.getInt32(offset + 8, true);
+            let bcPlanes = dataView.getUint16(offset + 12, true);
+            let bcBitCount = dataView.getUint16(offset + 14, true);
+            let biCompression = dataView.getUint16(offset + 16, true);
+            let biSizeImage = dataView.getUint32(offset + 20, true);
+            console.log(bcWidth, bcHeight, bcPlanes, bcBitCount);
+            console.log(biCompression, biSizeImage);
+        }
+        offsetX += files[i].width;
+    }
 }
 
 document.body.addEventListener('drop', e => {
