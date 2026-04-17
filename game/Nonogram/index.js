4a459342db5734f43e4b78840ee3855405624b2b
sapphire-al2o3 <t_telkun@hotmail.com>
2026-04-17 21:59:24 +0900

    数字部分のセルを作成

--- game/Nonogram/index.js
+++ game/Nonogram/index.js
@@ -3,10 +3,37 @@ const cells = [];
 const elems = [];
 const sizeX = 10;
 const sizeY = 10;
+const hintSizeX = sizeX / 2 ^ 0;
+const hintSizeY = sizeY / 2 ^ 0;
+
+
+
+for (let i = 0; i < hintSizeY; i++) {
+    const tr = document.createElement('tr');
+
+    for (let j = 0; j < hintSizeX; j++) {
+        const th = document.createElement('th');
+        tr.appendChild(th);
+    }
+
+    for (let j = 0; j < sizeX; j++) {
+        const th = document.createElement('th');
+        th.textContent = '0';
+        tr.appendChild(th);
+    }
+    table.appendChild(tr);
+}
 
 let k = 0;
 for (let i = 0; i < sizeY; i++) {
     const tr = document.createElement('tr');
+
+    for (let j = 0; j < hintSizeX; j++) {
+        const th = document.createElement('th');
+        th.textContent = '0';
+        tr.appendChild(th);
+    }
+
     for (let j = 0; j < sizeX; j++) {
         const td = document.createElement('td');
         td.setAttribute('x', j);
